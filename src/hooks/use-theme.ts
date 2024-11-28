6d2b1eec50e6cffe5036f33f09c2bed11be387e5
Daustinn <daustinn.music@gmail.com>
2024-11-28 15:36:07 -0500

    Add useTheme hook for creating Fluent UI themes

---
+++ src/hooks/use-theme.ts
@@ -0,0 +1,9 @@
+import { useMemo } from 'react'
+
+import createFluentTheme from '@/fluentui/create-theme'
+type ThemePreference = 'dark' | 'light'
+
+const useTheme = (themePreference: ThemePreference) =>
+  useMemo(() => createFluentTheme(themePreference), [themePreference])
+
+export default useTheme
