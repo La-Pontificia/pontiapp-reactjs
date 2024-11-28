0ee489cbb758ec5abab8b2d39da9c79e6f07ab87
Daustinn <daustinn.music@gmail.com>
2024-11-28 15:35:57 -0500

    Add error constants and business/module data

---
+++ src/const/errors.ts
@@ -0,0 +1,7 @@
+export const ERRORS = {
+  unauthorized:
+    'Tu cuenta no se encuentra sincronizada con el sistema, por favor contacta a soporte.',
+  auth_failed:
+    'Lo sentimos, no pudimos autenticarte. Por favor, intenta mas tarde.',
+  inactive: 'Tu cuenta se encuentra inactiva, por favor contacta a soporte.'
+} as const
---
+++ src/const/index.ts
@@ -0,0 +1,34 @@
+export const businesses = {
+  'https://www.elp.edu.pe': {
+    logo: '/businesses/elp.webp',
+    acronym: 'ELP',
+    name: 'Escuela Superior La Pontificia'
+  },
+  'https://www.ilp.edu.pe': {
+    logo: '/businesses/ilp.webp',
+    acronym: 'ILP',
+    name: 'Instituo La Pontificia'
+  },
+  'https://www.cybernet.edu.pe': {
+    logo: '/businesses/cc.webp',
+    acronym: 'CC',
+    name: 'Colegio Cybernet'
+  },
+  'https://www.idiomaslp.edu.pe/': {
+    logo: '/businesses/il.webp',
+    acronym: 'IL',
+    name: 'Idiomas La Pontificia'
+  },
+  'https://www.continualp.edu.pe/': {
+    logo: '/businesses/ec.webp',
+    acronym: 'EC',
+    name: 'Educacion Continua La Pontificia'
+  }
+} as const
+
+export const modules = {
+  '/modules/collaborators': {
+    name: 'Gestión Colaboradores',
+    icon: '/modules/collaborators.webp'
+  }
+}
