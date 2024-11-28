7ef39617c9a5d3eb699a2c72dbf1d2905ce71982
Daustinn <daustinn.music@gmail.com>
2024-11-28 15:36:41 -0500

    Add AuthProvider component for handling authentication state and actions

---
+++ src/store/auth.tsx
@@ -0,0 +1,64 @@
+import { api } from '@/lib/api'
+import { User } from '@/types/use'
+import React, { createContext } from 'react'
+
+interface AuthState {
+  user: User
+  privileges?: string[]
+  loading?: boolean
+  signOut?: () => void
+}
+
+const AuthContext = createContext<AuthState>({} as AuthState)
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuth = () => React.useContext(AuthContext)
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = React.useState<User | null>(null)
+  const [privileges, setPrivileges] = React.useState<string[]>([])
+  const [loading, setLoading] = React.useState(true)
+
+  const fetchAuth = async () => {
+    try {
+      const userData = await api.get<User>('auth/current')
+      const userInstance = new User(userData)
+      setUser(userInstance)
+      setPrivileges(userInstance.role.privileges || [])
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (err) {
+      setUser(null)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const signOut = async () => {
+    setLoading(true)
+    try {
+      await api.post('auth/signout')
+      setUser(null)
+      window.location.href = '/login'
+    } catch (error) {
+      console.error(error)
+      setLoading(false)
+    }
+  }
+
+  React.useEffect(() => {
+    void fetchAuth()
+  }, [])
+
+  return (
+    <AuthContext.Provider
+      value={{
+        privileges,
+        user: user as User,
+        loading,
+        signOut
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
