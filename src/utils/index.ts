a7c84ea85169a78ce3f7884ea445db00b1429595
Daustinn <daustinn.music@gmail.com>
2024-11-29 22:12:07 -0500

    Refactor CSS to include Geist font and autofill styles

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -11,3 +11,13 @@ export function cn(...inputs: ClassValue[]) {
 export const handleAuthError = (error: any) => {
   return ERRORS[error as keyof typeof ERRORS]
 }
+
+export const generateRandomPassword = () => {
+  const chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789'
+  let pass = ''
+  for (let x = 0; x < 8; x++) {
+    const i = Math.floor(Math.random() * chars.length)
+    pass += chars.charAt(i)
+  }
+  return pass
+}
