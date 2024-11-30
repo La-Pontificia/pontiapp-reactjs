7d8ffe00935a597fc8cb582120962bb9b62e6b8b
Daustinn <daustinn.music@gmail.com>
2024-11-29 22:11:31 -0500

    Add calendar strings and days constants

--- src/const/index.ts
+++ src/const/index.ts
@@ -31,4 +31,141 @@ export const modules = {
     name: 'Gestión Colaboradores',
     icon: '/modules/collaborators.webp'
   }
+} as const
+
+export const calendarStrings = {
+  days: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
+  goToToday: 'Ir a hoy',
+  months: [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre'
+  ],
+  shortDays: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
+  shortMonths: [
+    'Ene',
+    'Feb',
+    'Mar',
+    'Abr',
+    'May',
+    'Jun',
+    'Jul',
+    'Ago',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dic'
+  ]
+}
+
+export const days = {
+  '1': {
+    label: 'Lunes',
+    short: 'Lun'
+  },
+  '2': {
+    label: 'Martes',
+    short: 'Mar'
+  },
+  '3': {
+    label: 'Miércoles',
+    short: 'Mié'
+  },
+  '4': {
+    label: 'Jueves',
+    short: 'Jue'
+  },
+  '5': {
+    label: 'Viernes',
+    short: 'Vie'
+  },
+  '6': {
+    label: 'Sábado',
+    short: 'Sáb'
+  },
+  '7': {
+    label: 'Domingo',
+    short: 'Dom'
+  }
+} as const
+
+export const availableDomains = {
+  'lapontificia.edu.pe': 'La Pontificia'
+} as const
+
+export const AREA_DEVELOPER_ID = import.meta.env.VITE_AREA_DEVELOPER_ID
+export const DEPARTMENT_DEVELOPER_ID = import.meta.env
+  .VITE_DEPARTMENT_DEVELOPER_ID
+export const JOB_DEVELOPER_ID = import.meta.env.VITE_JOB_DEVELOPER_ID
+export const ROLE_DEVELOPER_ID = import.meta.env.VITE_ROLE_DEVELOPER_ID
+export const PRIVILEGE_DEVELOPER = import.meta.env.VITE_PRIVILEGE_DEVELOPER
+
+export const PRIVILEGES = {
+  development: 'Development',
+
+  // Collaborators management
+  'users:show': 'Ver usuarios',
+  'users:create': 'Registrar usuarios',
+  'users:edit': 'Editar usuarios',
+  'users:editYourself': 'Editar su propio usuario',
+  'users:asignCustomPrivileges': 'Asignar privilegios personalizados',
+  'users:asignSupervisor': 'Asignar supervisor a usuarios',
+  'users:toggleStaus': 'Deshabilitar o habilitar usuarios',
+  'users:resetPassword': 'Restablecer contraseñas',
+  'users:report': 'Generar reportes de usuarios',
+  'users:roles': 'Roles y permisos',
+
+  // Assistances management
+  'assists:centralized': 'Asistencias centralizadas',
+  'assists:centralizedWithoutCalculating':
+    'Asistencias centralizadas sin calcular',
+  'assists:withoutCalculating': 'Asistencias sin calcular',
+  'assists:singleSummary': 'Resumen único de asistencias',
+  'assists:report': 'Generar reportes de asistencias',
+  'assists:assistTerminals': 'Terminales de asistencias',
+
+  // Edas management
+
+  'edas:showAll': 'Ver todas las edas',
+  'edas:show': 'Ver edas que supervisa',
+  'edas:createAll': 'Registrar todas las edas',
+  'edas:create': 'Registrar edas que supervisa',
+  'edas:createYourself': 'Registrar sus edas',
+  'edas:reset': 'Resetear edas',
+  'edas:closeAll': 'Cerrar todas las edas',
+  'edas:close': 'Cerrar las edas que supervisa',
+  'edas:report': 'Generar reportes de edas',
+  'edas:years': 'Años',
+  'edas:goals': 'Objetivos',
+  'edas:evaluations': 'Evaluaciones',
+  'edas:questionnaires': 'Cuestionarios',
+  'edas:questionnaireTemplates': 'Plantillas de cuestionarios',
+
+  // Events management
+  'events:show': 'Ver eventos',
+  'events:edit': 'Editar eventos',
+  'events:create': 'Crear eventos',
+  'events:delete': 'Eliminar eventos',
+  'events:assists:register': 'Registrar asistencias a eventos',
+  'events:assists:view': 'Ver asistencias a eventos',
+  'events:assists:report': 'Generar reportes de asistencias a eventos',
+
+  // Audits management
+  audits: 'Auditoria del sistema',
+
+  // Reports management
+  'reports:showAll': 'Ver y descargar todos los reportes',
+  'reports:showYourSelf': 'Ver y descargar sus reportes',
+
+  // Settings management
+  settings: 'Acceso a configuraciones del sistema'
 }
