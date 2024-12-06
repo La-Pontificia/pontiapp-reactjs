134fe06c3450a51e7bb02f674ba19252859c8c94
Daustinn <daustinn.music@gmail.com>
2024-12-06 08:46:50 -0500

    Refactor TanstackProvider to set a garbage collection time for queries

--- src/providers/tanstack.tsx
+++ src/providers/tanstack.tsx
@@ -1,6 +1,12 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 1000 * 60 * 60
+    }
+  }
+})
 
 export default function TanstackProvider({
   children
