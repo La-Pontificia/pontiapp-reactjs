e62a2a29c5511b2fa40d69c5c3c881d0f1fc0981
Daustinn <daustinn.music@gmail.com>
2024-12-06 08:47:17 -0500

    Refactor package.json and pnpm-lock.yaml to include new dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      keyframes: {
+        slideIn: {
+          from: {
+            transform: 'translateY(100%)'
+          },
+          to: { transform: 'translateY(0px)' }
+        },
+        slideOut: {
+          from: { transform: 'translateY(0px)' },
+          to: { transform: 'translateY(100%)' }
+        },
+        swipeOut: {
+          from: { transform: 'translateY(var(--radix-toast-swipe-end-y))' },
+          to: { transform: 'translateY(100%)' }
+        }
+      },
+      animation: {
+        hide: 'hide 200ms ease-in',
+        slideIn: 'slideIn 200ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideOut: 'slideOut 200ms cubic-bezier(0.16, 1, 0.3, 1)',
+        swipeOut: 'swipeOut 200ms ease-out'
+      }
+    }
   },
   darkMode: 'media',
   plugins: []
