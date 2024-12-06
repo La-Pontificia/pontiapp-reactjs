8119475acb121dd0cb5875e3e5c4e637549072ad
Daustinn <daustinn.music@gmail.com>
2024-12-06 08:47:13 -0500

    Refactor handleAuthError and handleError functions in utils/index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ERRORS } from '@/const/errors'
 
 import clsx, { type ClassValue } from 'clsx'
@@ -7,9 +8,14 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const handleAuthError = (error: any) => {
-  return ERRORS[error as keyof typeof ERRORS]
+  const message = ERRORS[error as keyof typeof ERRORS]
+  return message || error
+}
+
+export const handleError = (error: any) => {
+  const message = ERRORS[error as keyof typeof ERRORS]
+  return message || error
 }
 
 export const generateRandomPassword = () => {
