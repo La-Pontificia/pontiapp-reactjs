2d1cc2bbf72da2b4f37a509a55787a7347c3b3b6
Daustinn <daustinn.music@gmail.com>
2024-12-08 21:46:13 -0500

    Refactor module layout and remove AuthProvider from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,20 +6,17 @@ import FluentUIProvider from '@/providers/fluentui'
 import ThemeProvider from '@/providers/theme'
 import TanstackProvider from './providers/tanstack'
 import UiProvider from './providers/ui'
-import { AuthProvider } from './store/auth'
 
-createRoot(document.getElementById('app')!).render(
-  <AuthProvider>
-    <UiProvider>
-      <ThemeProvider>
-        <FluentUIProvider>
-          <BrowserRouter>
-            <TanstackProvider>
-              <MainRoutes />
-            </TanstackProvider>
-          </BrowserRouter>
-        </FluentUIProvider>
-      </ThemeProvider>
-    </UiProvider>
-  </AuthProvider>
+createRoot(document.body).render(
+  <UiProvider>
+    <ThemeProvider>
+      <FluentUIProvider>
+        <BrowserRouter>
+          <TanstackProvider>
+            <MainRoutes />
+          </TanstackProvider>
+        </BrowserRouter>
+      </FluentUIProvider>
+    </ThemeProvider>
+  </UiProvider>
 )
