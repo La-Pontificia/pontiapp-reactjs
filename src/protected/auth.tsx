d633398b1bb66f4252c3f530e98e4f5d4626c45d
Daustinn <daustinn.music@gmail.com>
2024-12-08 21:45:41 -0500

    Refactor Protected and ProtectedModule components

---
+++ src/protected/auth.tsx
@@ -0,0 +1,29 @@
+import { useAuth } from '@/store/auth'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router'
+
+export default function Protected({
+  children,
+  has,
+  navigate: navigateProp
+}: {
+  children: React.ReactNode
+  navigate: string
+  has: string
+}) {
+  const navigate = useNavigate()
+  const { user: authUser } = useAuth()
+  const [isChecking, setIsChecking] = useState(true)
+
+  useEffect(() => {
+    if (!authUser || !authUser.hasPrivilege(has)) {
+      navigate(navigateProp)
+    } else {
+      setIsChecking(false)
+    }
+  }, [authUser, has, navigate, navigateProp])
+
+  if (isChecking) return null
+
+  return <>{children}</>
+}
---
+++ src/protected/module.tsx
@@ -0,0 +1,29 @@
+import { useAuth } from '@/store/auth'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router'
+
+export default function ProtectedModule({
+  children,
+  has,
+  navigate: navigateProp
+}: {
+  children: React.ReactNode
+  navigate: string
+  has: string
+}): JSX.Element {
+  const navigate = useNavigate()
+  const { user: authUser } = useAuth()
+  const [isChecking, setIsChecking] = useState(true)
+
+  useEffect(() => {
+    if (!authUser || !authUser.hasModule(has)) {
+      navigate(navigateProp)
+    } else {
+      setIsChecking(false)
+    }
+  }, [authUser, has, navigate, navigateProp])
+
+  if (isChecking) return <></>
+
+  return <>{children}</>
+}
