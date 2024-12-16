b0318a4538aa39a58cc684f99ecc8ce2ed538c6a
Daustinn <daustinn.music@gmail.com>
2024-12-16 11:18:20 -0500

    Refactor import paths to use absolute imports in dayjs.ts and echo.ts Remove unused file uploads.ts

--- src/lib/dayjs.ts
+++ src/lib/dayjs.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Time } from '@/types'
+import { Time } from '~/types'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import dayjs from 'dayjs'
 import 'dayjs/locale/es'
@@ -68,4 +68,30 @@ export const createDateByTime = (time: Time): Date => {
   newDate.setSeconds(Number(seconds))
 
   return newDate
+}
+
+export function timeAgoShort(d: any): string {
+  const date = new Date(d)
+  const now = dayjs()
+  const targetDate = dayjs(date)
+  const diffInSeconds = now.diff(targetDate, 'second')
+
+  if (diffInSeconds < 60)
+    return `${diffInSeconds} segundo${diffInSeconds > 1 ? 's' : ''}`
+  const diffInMinutes = now.diff(targetDate, 'minute')
+  if (diffInMinutes < 60)
+    return `${diffInMinutes} minuto${diffInMinutes > 1 ? 's' : ''}`
+  const diffInHours = now.diff(targetDate, 'hour')
+  if (diffInHours < 24)
+    return `${diffInHours} hora${diffInHours > 1 ? 's' : ''}`
+  const diffInDays = now.diff(targetDate, 'day')
+  if (diffInDays < 7) return `${diffInDays} día${diffInDays > 1 ? 's' : ''}`
+  const diffInWeeks = now.diff(targetDate, 'week')
+  if (diffInWeeks < 4)
+    return `${diffInWeeks} semana${diffInWeeks > 1 ? 's' : ''}`
+  const diffInMonths = now.diff(targetDate, 'month')
+  if (diffInMonths < 12)
+    return `${diffInMonths} mes${diffInMonths > 1 ? 'es' : ''}`
+  const diffInYears = now.diff(targetDate, 'year')
+  return `${diffInYears} año${diffInYears > 1 ? 's' : ''}`
 }
---
+++ src/lib/echo.ts
@@ -0,0 +1,23 @@
+import Echo from 'laravel-echo'
+import Pusher from 'pusher-js'
+
+declare global {
+  interface Window {
+    Pusher: typeof Pusher
+  }
+}
+
+window.Pusher = Pusher
+
+const echo = new Echo({
+  broadcaster: 'reverb',
+  key: import.meta.env.VITE_REVERB_APP_KEY as string,
+  wsHost: import.meta.env.VITE_REVERB_HOST as string,
+  wsPort: parseInt(import.meta.env.VITE_REVERB_PORT ?? '80'),
+  wssPort: parseInt(import.meta.env.VITE_REVERB_PORT ?? '443'),
+  forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+  enabledTransports: ['ws', 'wss'],
+  disableStats: true
+})
+
+export default echo
