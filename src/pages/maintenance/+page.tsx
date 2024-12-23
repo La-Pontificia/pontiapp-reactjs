658087b97f685f86e1999039d19e6ffe49f20c95
Daustinn <daustinn.music@gmail.com>
2024-12-23 01:29:49 -0500

    Add MaintenancePage component

---
+++ src/pages/maintenance/+page.tsx
@@ -0,0 +1,15 @@
+export default function MaintenancePage() {
+  return (
+    <div className="min-h-svh grid place-content-center text-center bg-neutral-900">
+      <div className="max-w-sm mx-auto">
+        <h1 className="pb-2 text-lg font-medium tracking-tight">
+          Estamos actualizando la plataforma
+        </h1>
+        <p className="text-xs opacity-50">
+          Por favor, vuelve a intentar en unos minutos. Gracias por tu
+          comprensión.
+        </p>
+      </div>
+    </div>
+  )
+}
