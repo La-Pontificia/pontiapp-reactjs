2bb3cb39edc8b9202d05e92a38a846488b54033a
Daustinn <daustinn.music@gmail.com>
2024-12-31 13:10:37 -0500

    Refactor table row styles in event and record components

--- src/pages/modules/events/report-files/+page.tsx
+++ src/pages/modules/events/report-files/+page.tsx
@@ -42,7 +42,7 @@ export default function EventsReportFilesPage() {
 
   return (
     <div className="w-full h-full flex flex-col">
-      <p className="p-3 text-xs dark:text-blue-500">
+      <p className="p-3 text-xs dark:text-blue-500 text-blue-700">
         Archivos de reportes de registros a eventos.
       </p>
       {reports && reports?.length < 1 ? (
@@ -84,7 +84,7 @@ export default function EventsReportFilesPage() {
                         {items.map((item) => (
                           <tr
                             key={item.id}
-                            className="bg-stone-900 [&>td]:px-4 first:[&>td]:first:rounded-tl-xl last:[&>td]:first:rounded-tr-xl first:[&>td]:last:rounded-bl-xl last:[&>td]:last:rounded-br-xl"
+                            className="bg-stone-100 dark:bg-stone-900 [&>td]:px-4 first:[&>td]:first:rounded-tl-xl last:[&>td]:first:rounded-tr-xl first:[&>td]:last:rounded-bl-xl last:[&>td]:last:rounded-br-xl"
                           >
                             <td>
                               <div className="flex items-center gap-2 py-3">
--- src/pages/modules/events/sidebar.tsx
+++ src/pages/modules/events/sidebar.tsx
@@ -31,6 +31,7 @@ type ItemNav = {
   children?: React.ReactNode
   emptyIcon?: boolean
 }
+
 const ItemNav = (props: ItemNav) => {
   const { pathname } = useLocation()
 
@@ -45,18 +46,18 @@ const ItemNav = (props: ItemNav) => {
     <Link
       data-active={isActive ? '' : undefined}
       to={props.href}
-      className="block relative dark:text-neutral-300 text-neutral-700 data-[active]:font-semibold group pl-3"
+      className="block relative dark:text-neutral-300 text-neutral-900 data-[active]:font-semibold group pl-3"
     >
       <div className="absolute pointer-events-none inset-y-0 left-0 flex items-center">
         <span className="h-[20px] group-data-[active]:bg-blue-800 dark:group-data-[active]:bg-blue-500 group-data-[active]:opacity-100 w-[3px] rounded-full bg-stone-500/30 group-hover:opacity-100 opacity-0" />
       </div>
-      <div className="flex items-center group-data-[active]:dark:text-white transition-all gap-2 px-2 py-2 rounded-lg group-hover:bg-stone-200 dark:group-hover:bg-stone-500/20">
+      <div className="flex items-center group-data-[active]:dark:text-white  gap-2 px-2 py-2 rounded-lg group-hover:bg-stone-100 dark:group-hover:bg-stone-200 dark:group-hover:bg-stone-500/20">
         {props.emptyIcon ? (
           <span className="w-[24px] aspect-square"></span>
         ) : Icon ? (
           <Icon
             fontSize={24}
-            className="group-data-[active]:text-blue-500 dark:text-stone-400 group-data-[active]:dark:text-blue-500"
+            className="dark:text-stone-400 group-data-[active]:dark:text-blue-500 group-data-[active]:text-blue-800"
           />
         ) : (
           <Avatar
