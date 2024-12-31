720ce7f9b7f3edfc87c942cdf19e495d8e7e818c
Daustinn <daustinn.music@gmail.com>
2024-12-31 13:10:56 -0500

    Refactor component styles and class names in user grids, tables, and sidebar

--- src/providers/fluentui.tsx
+++ src/providers/fluentui.tsx
@@ -1,24 +1,20 @@
-import { customDarkTheme } from '~/themes'
 import {
-  FluentProvider
-  // webDarkTheme,
-  // webLightTheme
+  FluentProvider,
+  webDarkTheme,
+  webLightTheme
 } from '@fluentui/react-components'
-// import { useTheme } from 'next-themes'
-// import useMediaQuery from 'beautiful-react-hooks/useMediaQuery'
 import React from 'react'
+import { useTheme } from './theme'
 
 export default function FluentUIProvider({
   children
 }: Readonly<{
   children?: React.ReactNode
 }>) {
-  // const { theme } = useTheme()
-  // const [darkMode] = React.useState(
-  //   useMediaQuery('(prefers-color-scheme: dark)')
-  // )
-
-  // const t = theme === 'system' ? (darkMode ? 'dark' : 'light') : theme
-
-  return <FluentProvider theme={customDarkTheme}>{children}</FluentProvider>
+  const { theme } = useTheme()
+  return (
+    <FluentProvider theme={theme === 'dark' ? webDarkTheme : webLightTheme}>
+      {children}
+    </FluentProvider>
+  )
 }
--- src/providers/theme.tsx
+++ src/providers/theme.tsx
@@ -1,8 +1,51 @@
-import { ThemeProvider as PrimitiveThemeProvider } from 'next-themes'
+/* eslint-disable react-refresh/only-export-components */
+import React from 'react'
+import { useLocalStorage } from 'usehooks-ts'
+
+type Theme = 'light' | 'dark'
+
+type ThemeProviderProps = {
+  theme: Theme
+  setTheme: (theme: Theme) => void
+  toggleTheme: () => void
+}
+
+const ThemeContext = React.createContext<ThemeProviderProps>(
+  {} as ThemeProviderProps
+)
+
+export const useTheme = () => React.useContext(ThemeContext)
+
 export default function ThemeProvider({
   children
 }: Readonly<{
   children?: React.ReactNode
 }>) {
-  return <PrimitiveThemeProvider>{children}</PrimitiveThemeProvider>
+  const [theme, set] = useLocalStorage<Theme>('theme', 'light')
+  const isDark = theme === 'dark'
+
+  React.useEffect(() => {
+    document.body.classList.remove('light', 'dark')
+    document.body.classList.add(isDark ? 'dark' : 'light')
+  }, [isDark])
+
+  const toggleTheme = () => {
+    set(theme === 'light' ? 'dark' : 'light')
+  }
+
+  const setTheme = (theme: Theme) => {
+    set(theme)
+  }
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        setTheme,
+        theme,
+        toggleTheme
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  )
 }
