d378e42aa56b296f0d94b6b1d9fc2d35f8585f2a
Daustinn <daustinn.music@gmail.com>
2024-12-31 13:10:52 -0500

    Refactor component styles and class names in user grids, tables, and sidebar

--- src/pages/+loading.tsx
+++ src/pages/+loading.tsx
@@ -2,7 +2,7 @@ import { Spinner } from '@fluentui/react-components'
 
 export default function RootLoading() {
   return (
-    <div className="min-h-svh flex flex-col dark:bg-[#1b1a19]">
+    <div className="min-h-svh flex flex-col bg-white text-black dark:text-stone-300 dark:bg-[#1b1a19]">
       <div></div>
       <div className="flex-grow flex items-center justify-center">
         <Spinner size="huge" />
