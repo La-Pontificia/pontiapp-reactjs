34b9a2c131e8e155a472401cc6c276f13be63c89
Daustinn <daustinn.music@gmail.com>
2025-01-08 10:57:43 -0500

    Refactor attention ticket answer page components and styles

---
+++ src/pages/modules/attentions/business-units/+page.tsx
@@ -0,0 +1,169 @@
+import { api } from '~/lib/api'
+import { useQuery } from '@tanstack/react-query'
+import { GlobeRegular } from '@fluentui/react-icons'
+import { Avatar, Button, Checkbox, Spinner } from '@fluentui/react-components'
+import React from 'react'
+import { BusinessUnit } from '~/types/business-unit'
+import { useAuth } from '~/store/auth'
+import { toast } from '~/commons/toast'
+import { handleError } from '~/utils'
+
+export default function AttentionsBusinessUnitsPage() {
+  const { user: authUser } = useAuth()
+  const [fetching, setFetching] = React.useState(false)
+  const {
+    data,
+    isLoading: isLoading,
+    refetch
+  } = useQuery<{
+    atttentionBusinessUnits: BusinessUnit[]
+    businessUnits: BusinessUnit[]
+  }>({
+    queryKey: ['attentions/businessUnits', 'all'],
+    queryFn: async () => {
+      const res = await api.get<{
+        atttentionBusinessUnits: BusinessUnit[]
+        businessUnits: BusinessUnit[]
+      }>('attentions/businessUnits')
+      if (!res.ok)
+        return {
+          atttentionBusinessUnits: [],
+          businessUnits: []
+        }
+      return {
+        atttentionBusinessUnits: res.data.atttentionBusinessUnits.map(
+          (b) => new BusinessUnit(b)
+        ),
+        businessUnits: res.data.businessUnits.map((b) => new BusinessUnit(b))
+      }
+    }
+  })
+
+  const [selectedIds, setSelectedIds] = React.useState<string[]>([])
+
+  React.useEffect(() => {
+    if (data?.atttentionBusinessUnits) {
+      setSelectedIds(data.atttentionBusinessUnits.map((b) => b.id))
+    }
+  }, [data?.atttentionBusinessUnits])
+
+  const onUpdate = async () => {
+    setFetching(true)
+    const res = await api.post('attentions/businessUnits', {
+      data: JSON.stringify({
+        ids: selectedIds
+      })
+    })
+
+    if (!res.ok) {
+      toast(handleError(res.error))
+    } else {
+      refetch()
+      toast('Unidades de negocio actualizadas correctamente')
+    }
+    setFetching(false)
+  }
+
+  return (
+    <div className="flex px-3 flex-col w-full pb-3 overflow-auto h-full">
+      <h1 className="pt-4 text-xs dark:text-blue-600 text-blue-500">
+        Selecciona las unidades de negocio que estarán disponibles para las
+        atenciones.
+      </h1>
+      <div className="overflow-auto flex-grow rounded-xl pt-2 h-full">
+        {isLoading ? (
+          <div className="h-full grid place-content-center">
+            <Spinner size="huge" />
+          </div>
+        ) : (
+          data && (
+            <div className="overflow-auto">
+              <table className="w-full">
+                <thead>
+                  <tr className="dark:text-neutral-400 text-xs font-semibold [&>td]:py-2 [&>td]:px-2">
+                    <td className="min-w-[50px] max-w-[50px] w-[50px]"></td>
+                    <td className="text-nowrap">Unidad</td>
+                    <td className="text-nowrap">Acrónimo</td>
+                    <td className="text-nowrap">Dominio</td>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-neutral-500/20">
+                  {data.businessUnits.map((business) => {
+                    const domainURL = business.domain.startsWith('https')
+                      ? business.domain
+                      : `https://${business.domain}`
+
+                    const isSelected = selectedIds.includes(business.id)
+                    return (
+                      <tr
+                        key={business.id}
+                        className="relative bg-neutral-50/40 dark:bg-neutral-900 odd:bg-neutral-500/10 dark:even:bg-neutral-500/20 [&>td]:text-nowrap  group [&>td]:p-2.5 [&>td]:px-3 first:[&>td]:first:rounded-tl-xl last:[&>td]:first:rounded-tr-xl first:[&>td]:last:rounded-bl-xl last:[&>td]:last:rounded-br-xl"
+                      >
+                        <td>
+                          <div>
+                            <Checkbox
+                              checked={isSelected}
+                              onChange={(_, state) => {
+                                if (state.checked) {
+                                  setSelectedIds([...selectedIds, business.id])
+                                } else {
+                                  setSelectedIds(
+                                    selectedIds.filter(
+                                      (id) => id !== business.id
+                                    )
+                                  )
+                                }
+                              }}
+                            />
+                          </div>
+                        </td>
+                        <td>
+                          <div className="flex items-center gap-2">
+                            <Avatar
+                              name={business.name}
+                              color="colorful"
+                              size={32}
+                            />
+                            <p>{business.name}</p>
+                          </div>
+                        </td>
+                        <td>
+                          <p className="font-semibold">{business.acronym}</p>
+                        </td>
+                        <td>
+                          <a
+                            target="_blank"
+                            href={domainURL}
+                            className="flex items-center dark:text-blue-600 gap-1.5 hover:underline text-nowrap"
+                          >
+                            <GlobeRegular fontSize={20} />
+                            {domainURL}
+                          </a>
+                        </td>
+                      </tr>
+                    )
+                  })}
+                </tbody>
+              </table>
+              <footer className="py-3">
+                <Button
+                  disabled={
+                    selectedIds.length === 0 ||
+                    isLoading ||
+                    !authUser.hasPrivilege('attentions:businessUnits:update') ||
+                    selectedIds.length ===
+                      data?.atttentionBusinessUnits.length ||
+                    fetching
+                  }
+                  onClick={onUpdate}
+                >
+                  Actualizar
+                </Button>
+              </footer>
+            </div>
+          )
+        )}
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/modules/attentions/business-units/service.tsx
@@ -0,0 +1,203 @@
+import {
+  Avatar,
+  Badge,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogBody,
+  DialogSurface,
+  DialogTitle,
+  DialogTrigger,
+  Spinner
+} from '@fluentui/react-components'
+import React from 'react'
+import { api } from '~/lib/api'
+import { handleError } from '~/utils'
+import { toast } from '~/commons/toast'
+
+import { DeleteFilled, PenFilled } from '@fluentui/react-icons'
+import Form from './form'
+import { useAuth } from '~/store/auth'
+import { AttentionService } from '~/types/attention-service'
+
+export default function Item({
+  item,
+  refetch
+}: {
+  item: AttentionService
+  refetch: () => void
+}) {
+  const [openDelete, setOpenDelete] = React.useState(false)
+  const [deleting, setDeleting] = React.useState(false)
+
+  const { user: authUser } = useAuth()
+
+  const handleDelete = async () => {
+    setDeleting(true)
+    const res = await api.post(`attentions/services/${item?.id}/delete`)
+    if (!res.ok) {
+      setDeleting(false)
+      return toast(handleError(res.error))
+    }
+    setDeleting(false)
+    setOpenDelete(false)
+    toast(`${item.name} eliminado correctamente`)
+    refetch()
+  }
+
+  return (
+    <>
+      <tr className="relative bg-neutral-50/40 dark:bg-neutral-900 odd:bg-neutral-500/10 dark:even:bg-neutral-500/20 [&>td]:text-nowrap  group [&>td]:p-2.5 [&>td]:px-3 first:[&>td]:first:rounded-tl-xl last:[&>td]:first:rounded-tr-xl first:[&>td]:last:rounded-bl-xl last:[&>td]:last:rounded-br-xl">
+        <td>
+          <div className="flex items-center gap-2">
+            <Avatar name={item.name} color="colorful" size={32} />
+            <p>{item.name}</p>
+          </div>
+        </td>
+        <td>
+          <p>
+            <span className="opacity-60 inline-block pr-1">
+              {item.position?.shortName}
+            </span>
+            {item.position?.name}
+          </p>
+        </td>
+        <td>
+          <p>{item.position?.business.name}</p>
+        </td>
+        <td>
+          <div className="flex items-center gap-2">
+            {authUser.hasPrivilege('attentions:services:edit') && (
+              <Form
+                defaultValues={item}
+                refetch={refetch}
+                triggerProps={{
+                  size: 'small',
+                  appearance: 'transparent',
+                  children: (
+                    <Badge
+                      icon={<PenFilled fontSize={15} />}
+                      appearance="tint"
+                      color="success"
+                    >
+                      Editar
+                    </Badge>
+                  )
+                }}
+              />
+            )}
+            {authUser.hasPrivilege('attentions:services:delete') && (
+              <button onClick={() => setOpenDelete(true)}>
+                <Badge
+                  icon={<DeleteFilled fontSize={15} />}
+                  appearance="tint"
+                  color="danger"
+                >
+                  Eliminar
+                </Badge>
+              </button>
+            )}
+          </div>
+        </td>
+        {/* <td>
+          <div className="flex items-center gap-2 py-3">
+            <Avatar size={40} icon={<PersonDesktopRegular />} />
+            <p className="text-sm line-clamp-1 opacity-90">{item.name}</p>
+          </div>
+        </td>
+        <td>
+          {item.current ? (
+            <div className="flex items-center gap-2 py-3">
+              <Avatar
+                size={24}
+                color="colorful"
+                image={{
+                  src: item.current.displayName
+                }}
+                name={item.current.displayName}
+              />
+              <p className="text-sm line-clamp-1 opacity-90">
+                {item.current.displayName}
+              </p>
+            </div>
+          ) : (
+            <p className="text-sm line-clamp-1 opacity-90">-</p>
+          )}
+        </td>
+        <td>
+          {item.available ? (
+            <Badge color="success" appearance="tint">
+              Disponible
+            </Badge>
+          ) : (
+            <Badge color="subtle" appearance="tint">
+              Inactivo
+            </Badge>
+          )}
+        </td>
+        <td>
+          <div className="flex items-center gap-2">
+            {authUser.hasPrivilege('attentions:services:edit') && (
+              <Form
+                defaultValues={item}
+                refetch={refetch}
+                triggerProps={{
+                  size: 'small',
+                  appearance: 'transparent',
+                  children: (
+                    <Badge
+                      icon={<PenFilled fontSize={15} />}
+                      appearance="tint"
+                      color="success"
+                    >
+                      Editar
+                    </Badge>
+                  )
+                }}
+              />
+            )}
+            {authUser.hasPrivilege('attentions:services:delete') && (
+              <button onClick={() => setOpenDelete(true)}>
+                <Badge
+                  icon={<DeleteFilled fontSize={15} />}
+                  appearance="tint"
+                  color="danger"
+                >
+                  Eliminar
+                </Badge>
+              </button>
+            )}
+          </div>
+        </td> */}
+      </tr>
+      {openDelete && (
+        <Dialog
+          open={openDelete}
+          onOpenChange={(_, e) => setOpenDelete(e.open)}
+          modalType="alert"
+        >
+          <DialogSurface>
+            <DialogBody>
+              <DialogTitle>
+                ¿Estás seguro de eliminar la opción: {item.name}?
+              </DialogTitle>
+              <DialogActions>
+                <DialogTrigger disableButtonEnhancement>
+                  <Button appearance="secondary">Cancelar</Button>
+                </DialogTrigger>
+                <Button
+                  onClick={handleDelete}
+                  disabled={deleting}
+                  icon={deleting ? <Spinner size="tiny" /> : undefined}
+                  appearance="primary"
+                >
+                  ELiminar
+                </Button>
+              </DialogActions>
+            </DialogBody>
+          </DialogSurface>
+        </Dialog>
+      )}
+    </>
+  )
+}
