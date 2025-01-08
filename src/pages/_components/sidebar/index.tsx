603357c8ac119299fec7458d2ef6fff5b63bdd04
Daustinn <daustinn.music@gmail.com>
2025-01-08 10:57:24 -0500

    Refactor sidebar icons and styles

--- src/pages/_components/sidebar/index.tsx
+++ src/pages/_components/sidebar/index.tsx
@@ -1,19 +1,19 @@
 import { useAuth } from '~/store/auth'
 import { Avatar, Tooltip } from '@fluentui/react-components'
 import {
-  ClockBillFilled,
-  ClockBillRegular,
-  DataHistogramFilled,
-  DataHistogramRegular,
+  ClockFilled,
+  ClockRegular,
+  // DataHistogramFilled,
+  // DataHistogramRegular,
   type FluentIcon,
   HomeFilled,
   HomeRegular,
-  MegaphoneLoudFilled,
-  MegaphoneLoudRegular,
-  PeopleEditFilled,
-  PeopleEditRegular,
-  TabletSpeakerFilled,
-  TabletSpeakerRegular
+  // MegaphoneLoudFilled,
+  // MegaphoneLoudRegular,
+  PeopleFilled,
+  PeopleRegular,
+  TabletFilled,
+  TabletRegular
 } from '@fluentui/react-icons'
 import { Link, useLocation } from 'react-router'
 import React from 'react'
@@ -49,17 +49,18 @@ const ItemNav = (props: ItemNav) => {
         onMouseLeave={() => setHover(false)}
         data-active={isActive ? '' : undefined}
         to={props.href}
-        className="relative px-1.5 group dark:text-stone-400 text-stone-600  flex justify-center items-center"
+        className="relative group dark:text-stone-400 text-stone-600  flex justify-center items-center"
       >
-        <div className="absolute inset-y-0 transition-all group-hover:py-6 group-data-[active]:py-4 py-7  left-0">
-          <div className="h-full w-[3px] group-hover:bg-[#1e38e2] group-hover:dark:bg-[#1e38e2]  group-data-[active]:bg-blue-600 group-data-[active]:dark:bg-[#1e38e2] rounded-xl"></div>
+        <div className="absolute inset-y-0 transition-all group-hover:py-5 group-data-[active]:py-4 py-7 left-0">
+          <div className="h-full w-[3px] group-hover:bg-[#1d02ec] group-hover:dark:bg-[#ffcdff] group-data-[active]:bg-[#1d02ec] group-data-[active]:dark:bg-[#ffcdff] rounded-xl"></div>
         </div>
-        <div className="w-[60px] min-w-[60px] flex group-data-[active]:shadow-sm border-transparent group-data-[active]:border group-data-[active]:border-stone-200 group-data-[active]:dark:border-stone-800 items-center flex-col justify-center aspect-[10/9] py-0.5 text-center rounded-[14px] group-data-[active]:bg-white group-hover:dark:bg-[#0c0b0d] hover:bg-white/60  group-data-[active]:dark:bg-[#0d0d0f]">
+        <div className="w-[57px] transition-colors min-w-[57px] flex group-data-[active]:shadow-sm border-transparent border group-data-[active]:border-stone-200 group-data-[active]:dark:border-stone-700/70 items-center flex-col justify-center aspect-[10/9] text-center rounded-[14px] group-data-[active]:bg-white group-hover:dark:bg-[#1a1a1a] hover:bg-white/60 group-data-[active]:dark:bg-[#1a1a1a]">
           {props.image ? (
             <Avatar
               name={props.text}
               color="colorful"
-              size={24}
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              size={30 as any}
               className=""
               image={{
                 src: props.image
@@ -67,13 +68,11 @@ const ItemNav = (props: ItemNav) => {
             />
           ) : (
             <Icon
-              fontSize={25}
-              className="group-data-[active]:dark:text-[#381aff] group-data-[active]:text-[#1d02ec]"
+              fontSize={30}
+              className="group-data-[active]:dark:text-[#ffcdff] group-hover:dark:text-[#ffcdff] group-data-[active]:text-[#1d02ec]"
             />
           )}
-          <p className="text-[10px] pt-0.5 leading-4 line-clamp-1">
-            {props.text}
-          </p>
+          <p className="text-[10px] leading-4 line-clamp-1">{props.text}</p>
         </div>
       </Link>
     </Tooltip>
@@ -90,13 +89,13 @@ export const RootSidebar = () => {
           className="flex justify-center items-center gap-1"
         >
           <img
-            src="/_lp-only-logo.webp"
+            src="/favicon.svg"
             width={25}
             alt='Logo Lettras "La Pontificia"'
           />
         </Link>
       </nav>
-      <nav className="flex pl-1 flex-col space-y-2 justify-center">
+      <nav className="flex pl-1 flex-col space-y-4 justify-center">
         <ItemNav
           tooltip={authUser.displayName}
           text={authUser.displayName}
@@ -109,12 +108,12 @@ export const RootSidebar = () => {
           <ItemNav
             tooltip="Gestión de usuarios"
             text="Usuarios"
-            icon={PeopleEditRegular}
-            activeIcon={PeopleEditFilled}
+            icon={PeopleRegular}
+            activeIcon={PeopleFilled}
             href="/m/users"
           />
         )}
-        {authUser.hasModule('edas') && (
+        {/* {authUser.hasModule('edas') && (
           <ItemNav
             tooltip="Gestión de Edas"
             text="Edas"
@@ -122,17 +121,17 @@ export const RootSidebar = () => {
             activeIcon={DataHistogramFilled}
             href="/m/edas"
           />
-        )}
+        )} */}
         {authUser.hasModule('assists') && (
           <ItemNav
             tooltip="Gestión de asistencias"
             text="Asisten..."
-            icon={ClockBillRegular}
-            activeIcon={ClockBillFilled}
+            icon={ClockRegular}
+            activeIcon={ClockFilled}
             href="/m/assists"
           />
         )}
-        {authUser.hasModule('events') && (
+        {/* {authUser.hasModule('events') && (
           <ItemNav
             tooltip="Gestión de eventos"
             text="Eventos"
@@ -141,12 +140,13 @@ export const RootSidebar = () => {
             href="/m/events"
           />
         )}
+          */}
         {authUser.hasModule('attentions') && (
           <ItemNav
-            tooltip="Control de Atención"
-            text="Atención"
-            icon={TabletSpeakerRegular}
-            activeIcon={TabletSpeakerFilled}
+            tooltip="Control de atención de tickets"
+            text="Tickets"
+            icon={TabletRegular}
+            activeIcon={TabletFilled}
             href="/m/attentions"
           />
         )}
