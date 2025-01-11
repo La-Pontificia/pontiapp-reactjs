faed057cae47eb0ebc6b803c030e87160a132d12
Daustinn <daustinn.music@gmail.com>
2025-01-10 21:30:09 -0500

    Refactor useQrCodeReader hook to handle input focus and enter key press

--- src/hooks/use-qr-reader.ts
+++ src/hooks/use-qr-reader.ts
@@ -8,15 +8,22 @@ type QrReaderProps = {
 
 export const useQrCodeReader = (props?: QrReaderProps) => {
   const [capturedText, setCapturedText] = React.useState<string>('')
+  const inputRef = React.useRef<HTMLInputElement>(null)
   const { onEnter, disabled, defaultText } = props ?? {}
 
   React.useEffect(() => {
     if (disabled) return
 
     const handleKeyPress = (event: KeyboardEvent) => {
+      if (inputRef.current === document.activeElement) {
+        // Si el foco está en el input, no agregar manualmente texto
+        return
+      }
+
       if (event.key === 'Enter') {
         onEnter?.(capturedText)
         setCapturedText('')
+        inputRef.current?.focus()
       } else {
         setCapturedText((prev) => prev + event.key)
       }
@@ -28,5 +35,14 @@ export const useQrCodeReader = (props?: QrReaderProps) => {
     }
   }, [capturedText, onEnter, disabled, defaultText])
 
-  return { capturedText }
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCapturedText(event.target.value)
+  }
+
+  const handleEnter = () => {
+    onEnter?.(capturedText)
+    setCapturedText('')
+    inputRef.current?.focus()
+  }
+  return { capturedText, inputRef, onChange, handleEnter }
 }
