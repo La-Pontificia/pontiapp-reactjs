28498444d1da0971eb3a05d5d46481dc821b08f5
Daustinn <daustinn.music@gmail.com>
2025-01-14 22:55:41 -0500

    Refactor sidebar component to include LpIcon and update icons

--- src/pages/_components/sidebar/index.tsx
+++ src/pages/_components/sidebar/index.tsx
@@ -3,24 +3,23 @@ import { Avatar, Tooltip } from '@fluentui/react-components'
 import {
   ClockFilled,
   ClockRegular,
-  // DataHistogramFilled,
-  // DataHistogramRegular,
+  DocumentFlowchartFilled,
+  DocumentFlowchartRegular,
   type FluentIcon,
-  HomeFilled,
-  HomeRegular,
   MegaphoneLoudFilled,
   MegaphoneLoudRegular,
-  PeopleFilled,
-  PeopleRegular,
+  PersonFilled,
+  PersonRegular,
   TabletFilled,
   TabletRegular
 } from '@fluentui/react-icons'
 import { Link, useLocation } from 'react-router'
 import React from 'react'
+import { Lp } from '~/icons'
 
 type ItemNav = {
-  icon: FluentIcon
-  activeIcon: FluentIcon
+  icon?: FluentIcon
+  activeIcon?: FluentIcon
   href: string
   image?: string
   tooltip: string
@@ -55,21 +54,19 @@ const ItemNav = (props: ItemNav) => {
           <div className="h-full w-[3px] group-hover:bg-[#1d02ec] group-hover:dark:bg-[#ffcdff] group-data-[active]:bg-[#1d02ec] group-data-[active]:dark:bg-[#ffcdff] rounded-xl"></div>
         </div>
         <div className="w-[57px] transition-colors min-w-[57px] flex group-data-[active]:shadow-sm border-transparent border group-data-[active]:border-stone-200 group-data-[active]:dark:border-stone-700/70 items-center flex-col justify-center aspect-[10/9] text-center rounded-[14px] group-data-[active]:bg-white group-hover:dark:bg-[#1a1a1a] hover:bg-white/60 group-data-[active]:dark:bg-[#1a1a1a]">
-          {props.image ? (
+          {Icon ? (
+            <Icon
+              fontSize={30}
+              className="group-data-[active]:dark:text-[#ffcdff] group-hover:dark:text-[#ffcdff] group-hover:text-[#1d02ec] group-data-[active]:text-[#1d02ec]"
+            />
+          ) : (
             <Avatar
               name={props.text}
               color="colorful"
-              // eslint-disable-next-line @typescript-eslint/no-explicit-any
-              size={30 as any}
-              className=""
+              size={28}
               image={{
                 src: props.image
               }}
-            />
-          ) : (
-            <Icon
-              fontSize={30}
-              className="group-data-[active]:dark:text-[#ffcdff] group-hover:dark:text-[#ffcdff] group-data-[active]:text-[#1d02ec]"
             />
           )}
           <p className="text-[10px] leading-4 line-clamp-1">{props.text}</p>
@@ -82,46 +79,40 @@ const ItemNav = (props: ItemNav) => {
 export const RootSidebar = () => {
   const { user: authUser } = useAuth()
   return (
-    <header className="flex flex-col min-w-[70px] relative justify-between z-[1] overflow-x-hidden overflow-y-auto py-5 h-full">
+    <header className="flex flex-col gap-5 min-w-[70px] relative justify-between z-[1] overflow-x-hidden overflow-y-auto py-5 h-full">
       <nav className="flex-grow basis-0">
         <Link
           to={`/${authUser.username}`}
-          className="flex justify-center items-center gap-1"
+          className="flex justify-center drop-shadow-[0_0_10px_rgba(0,0,0,.1)] dark:drop-shadow-[0_0_10px_rgba(0,0,0,.5)] items-center gap-1"
         >
-          <img
-            src="/favicon.svg"
-            width={25}
-            alt='Logo Lettras "La Pontificia"'
-          />
+          <Lp size={36} className="dark:text-violet-500 text-violet-700" />
         </Link>
       </nav>
-      <nav className="flex pl-1 flex-col space-y-4 justify-center">
+      <nav className="flex pl-1 flex-col flex-grow space-y-4 overflow-y-auto justify-center">
         <ItemNav
           tooltip={authUser.displayName}
           text={authUser.displayName}
           image={authUser.photoURL}
-          icon={HomeRegular}
-          activeIcon={HomeFilled}
           href={`/${authUser.username}`}
         />
         {authUser.hasModule('users') && (
           <ItemNav
             tooltip="Gestión de usuarios"
             text="Usuarios"
-            icon={PeopleRegular}
-            activeIcon={PeopleFilled}
+            icon={PersonRegular}
+            activeIcon={PersonFilled}
             href="/m/users"
           />
         )}
-        {/* {authUser.hasModule('edas') && (
+        {authUser.hasModule('edas') && (
           <ItemNav
             tooltip="Gestión de Edas"
             text="Edas"
-            icon={DataHistogramRegular}
-            activeIcon={DataHistogramFilled}
+            icon={DocumentFlowchartRegular}
+            activeIcon={DocumentFlowchartFilled}
             href="/m/edas"
           />
-        )} */}
+        )}
         {authUser.hasModule('assists') && (
           <ItemNav
             tooltip="Gestión de asistencias"
