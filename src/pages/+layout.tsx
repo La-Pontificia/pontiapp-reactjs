128b9e37df44419c89fcbbec409f97d0d93df52f
Daustinn <daustinn.music@gmail.com>
2025-01-22 13:05:54 -0500

    Refactor background color in RootLayout component

--- src/pages/+layout.tsx
+++ src/pages/+layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout() {
       >
         <div
           data-maximized={isModuleMaximized ? '' : undefined}
-          className="flex-grow relative max-w-[calc(100vw-74px)] data-[maximized]:max-w-full border dark:border-none shadow-[0_0_6px_rgba(0,0,0,.1)] dark:shadow-[0_0_6px_rgba(0,0,0,.5)] overflow-auto data-[maximized]:absolute data-[maximized]:z-[999] data-[maximized]:inset-0 rounded-xl data-[maximized]:rounded-none flex-col h-full flex overflow-y-auto w-full bg-[#f5f4f3] dark:bg-[#1f1f1f] text-black dark:text-neutral-100"
+          className="flex-grow relative max-w-[calc(100vw-74px)] data-[maximized]:max-w-full border dark:border-none shadow-[0_0_6px_rgba(0,0,0,.1)] dark:shadow-[0_0_6px_rgba(0,0,0,.5)] overflow-auto data-[maximized]:absolute data-[maximized]:z-[999] data-[maximized]:inset-0 rounded-xl data-[maximized]:rounded-none flex-col h-full flex overflow-y-auto w-full bg-[#f5f4f3] dark:bg-[#1f1e1d] text-black dark:text-neutral-100"
         >
           <RootHeader />
           <Outlet />
