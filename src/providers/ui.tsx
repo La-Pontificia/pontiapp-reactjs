8e03e8fc62d4368739925ed382b98f92e968907d
Daustinn <daustinn.music@gmail.com>
2025-01-25 00:10:37 -0500

    Refactor UIProvider component: Add UIContext and contentRef

--- src/providers/ui.tsx
+++ src/providers/ui.tsx
@@ -1,15 +1,27 @@
+/* eslint-disable react-refresh/only-export-components */
 import React from 'react'
 import { Toaster } from 'anni'
+
+type UIContextValue = {
+  contentRef: React.RefObject<HTMLDivElement>
+}
+
+export const UIContext = React.createContext<UIContextValue | null>(null)
 
 export default function UiProvider({
   children
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const contentRef = React.useRef<HTMLDivElement>(null)
   return (
-    <>
+    <UIContext.Provider
+      value={{
+        contentRef
+      }}
+    >
       <Toaster />
       {children}
-    </>
+    </UIContext.Provider>
   )
 }
