887eae1667c5eae7f1213affe65ca56974f52998
Daustinn <daustinn.music@gmail.com>
2025-01-29 05:10:32 -0500

    Refactor UserFeedback component: Add feedback functionality and UI

--- src/pages/_components/header/feedback.tsx
+++ src/pages/_components/header/feedback.tsx
@@ -1,14 +1,18 @@
 import { Tooltip } from '@fluentui/react-components'
 import { PersonFeedbackRegular } from '@fluentui/react-icons'
+import React from 'react'
+import Feedback from '~/components/feedback'
 
 export default function UserFeedback() {
+  const [open, setOpen] = React.useState(false)
   return (
     <>
       <Tooltip content="Envia un error o sugerencia" relationship="label">
-        <button>
+        <button className="feedback" onClick={() => setOpen(true)}>
           <PersonFeedbackRegular fontSize={23} />
         </button>
       </Tooltip>
+      <Feedback onOpenChange={setOpen} open={open} />
     </>
   )
 }
