ffad41444054b8f4d547621e591b13d86cab13cb
Daustinn <daustinn.music@gmail.com>
2025-01-29 05:12:17 -0500

    Refactor FluentUIProvider to use custom themes

--- src/providers/fluentui.tsx
+++ src/providers/fluentui.tsx
@@ -1,10 +1,7 @@
-import {
-  FluentProvider,
-  webDarkTheme,
-  webLightTheme
-} from '@fluentui/react-components'
+import { FluentProvider } from '@fluentui/react-components'
 import React from 'react'
 import { useTheme } from './theme'
+import { lightTheme, darkTheme } from '~/utils/themes'
 
 export default function FluentUIProvider({
   children
@@ -13,7 +10,7 @@ export default function FluentUIProvider({
 }>) {
   const { theme } = useTheme()
   return (
-    <FluentProvider theme={theme === 'dark' ? webDarkTheme : webLightTheme}>
+    <FluentProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
       {children}
     </FluentProvider>
   )
