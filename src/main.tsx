ff0e1e0031026be77591213c2e4cc0060e036e8c
Daustinn <daustinn.music@gmail.com>
2025-01-29 20:08:08 -0500

    Refactor main.tsx: Update import path for MainRoutes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router'
-import MainRoutes from '~/routes'
 import FluentUIProvider from '~/providers/fluentui'
 import ThemeProvider from '~/providers/theme'
 import TanstackProvider from './providers/tanstack'
 import UiProvider from './providers/ui'
+import MainRoutes from './pages/routes'
 
 createRoot(document.body).render(
   <ThemeProvider>
