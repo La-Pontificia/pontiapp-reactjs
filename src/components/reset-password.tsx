631f858059afb34ad9712b44c9212529a9573113
Daustinn <daustinn.music@gmail.com>
2025-01-30 16:16:18 -0500

    Refactor ResetPassword component: Add show success dialog and copy password functionality

--- src/components/reset-password.tsx
+++ src/components/reset-password.tsx
@@ -13,7 +13,7 @@ import {
   Persona,
   Spinner
 } from '@fluentui/react-components'
-import { Eye20Filled, Eye20Regular } from '@fluentui/react-icons'
+import { CopyRegular, Eye20Filled, Eye20Regular } from '@fluentui/react-icons'
 import { useMutation } from '@tanstack/react-query'
 import { toast } from 'anni'
 import React from 'react'
@@ -36,13 +36,15 @@ export default function ResetPassword({
   user: User
 }) {
   const [showPassword, setShowPassword] = React.useState(false)
+  const [openSuccess, setOpenSuccess] = React.useState(false)
   const [autoGeneratePassword, setAutoGeneratePassword] = React.useState(false)
 
-  const { control, handleSubmit, setValue, reset } = useForm<Values>({
-    defaultValues: {
+  const { control, handleSubmit, setValue, reset, watch } = useForm<Values>({
+    values: {
       password: '',
       requiredFirstLogin: true
-    }
+    },
+    reValidateMode: 'onChange'
   })
 
   const { mutate: resetPassword, isPending: reseting } = useMutation({
@@ -58,11 +60,10 @@ export default function ResetPassword({
       toast.error('Error al restablecer la contraseña ❌')
     },
     onSuccess: () => {
-      toast.success('Contraseña restablecida con éxito ✅')
-      reset()
       setShowPassword(false)
       setAutoGeneratePassword(false)
       setOpen(false)
+      setOpenSuccess(true)
     }
   })
 
@@ -70,105 +71,151 @@ export default function ResetPassword({
     resetPassword(values.password)
   })
 
+  const { password } = watch()
+
   return (
-    <Dialog
-      open={open}
-      onOpenChange={(_, e) => setOpen(e.open)}
-      modalType="modal"
-    >
-      <DialogSurface>
-        <DialogBody>
-          <DialogTitle>Restablecer contraseña</DialogTitle>
-          <DialogContent>
-            <div className="py-2">
-              <Persona
-                avatar={{
-                  image: {
-                    src: user.photoURL
-                  }
-                }}
-                name={user.displayName}
-                secondaryText={user.email}
-              />
-            </div>
-            <div className="grid pt-3 gap-2">
-              <Checkbox
-                onChange={(_, d) => {
-                  setAutoGeneratePassword(d.checked ? true : false)
-                  setValue(
-                    'password',
-                    d.checked ? generateRandomPassword() : ''
-                  )
-                }}
-                checked={autoGeneratePassword}
-                label="Auto-generar contraseña"
-              />
-              <Controller
-                control={control}
-                name="password"
-                rules={{ required: 'Requerido' }}
-                render={({ field, fieldState: { error } }) => (
-                  <Field
-                    label="Contraseña"
-                    required
-                    className="pl-9"
-                    validationMessage={error?.message}
-                    validationState={error ? 'error' : 'none'}
-                  >
-                    <Input
-                      input={{
-                        autoComplete: 'none'
+    <>
+      <Dialog
+        open={open}
+        onOpenChange={(_, e) => setOpen(e.open)}
+        modalType="modal"
+      >
+        <DialogSurface>
+          <DialogBody>
+            <DialogTitle>Restablecer contraseña</DialogTitle>
+            <DialogContent>
+              <div className="py-2">
+                <Persona
+                  avatar={{
+                    image: {
+                      src: user.photoURL
+                    }
+                  }}
+                  name={user.displayName}
+                  secondaryText={user.email}
+                />
+              </div>
+              <div className="grid pt-3 gap-2">
+                <Checkbox
+                  onChange={(_, d) => {
+                    setAutoGeneratePassword(d.checked ? true : false)
+                    setValue(
+                      'password',
+                      d.checked ? generateRandomPassword() : ''
+                    )
+                  }}
+                  checked={autoGeneratePassword}
+                  label="Auto-generar contraseña"
+                />
+                <Controller
+                  control={control}
+                  name="password"
+                  rules={{ required: 'Requerido' }}
+                  render={({ field, fieldState: { error } }) => (
+                    <Field
+                      label="Contraseña"
+                      required
+                      className="pl-9"
+                      validationMessage={error?.message}
+                      validationState={error ? 'error' : 'none'}
+                    >
+                      {open && (
+                        <Input
+                          {...field}
+                          input={{
+                            autoComplete: 'off'
+                          }}
+                          aria-hidden
+                          readOnly={autoGeneratePassword}
+                          type={showPassword ? 'text' : 'password'}
+                          contentAfter={
+                            <button
+                              onClick={() => setShowPassword((prev) => !prev)}
+                            >
+                              {showPassword ? (
+                                <Eye20Filled className="text-blue-500" />
+                              ) : (
+                                <Eye20Regular className="text-blue-500" />
+                              )}
+                            </button>
+                          }
+                        />
+                      )}
+                    </Field>
+                  )}
+                />
+                <Controller
+                  control={control}
+                  name="requiredFirstLogin"
+                  render={({ field }) => (
+                    <Checkbox
+                      onChange={(_, d) => {
+                        field.onChange(d.checked)
                       }}
-                      readOnly={autoGeneratePassword}
-                      type={showPassword ? 'text' : 'password'}
-                      contentAfter={
-                        <button
-                          onClick={() => setShowPassword((prev) => !prev)}
-                        >
-                          {showPassword ? (
-                            <Eye20Filled className="text-blue-500" />
-                          ) : (
-                            <Eye20Regular className="text-blue-500" />
-                          )}
-                        </button>
-                      }
-                      {...field}
-                      value={field.value}
+                      checked={field.value}
+                      label="Requerir cambio de contraseña en el primer inicio de sesión"
                     />
-                  </Field>
-                )}
-              />
-              <Controller
-                control={control}
-                name="requiredFirstLogin"
-                rules={{ required: 'Requerido' }}
-                render={({ field }) => (
-                  <Checkbox
-                    onChange={(_, d) => {
-                      field.onChange(d.checked)
-                    }}
-                    checked={field.value}
-                    label="Requerir cambio de contraseña en el primer inicio de sesión"
-                  />
-                )}
-              />
-            </div>
-          </DialogContent>
-          <DialogActions>
-            <DialogTrigger disableButtonEnhancement>
-              <Button appearance="secondary">Cancelar</Button>
-            </DialogTrigger>
-            <Button
-              onClick={handleResetPassword}
-              disabled={reseting}
-              icon={reseting ? <Spinner size="tiny" /> : undefined}
-              appearance="primary"
-            >
-              Restablecer
-            </Button>
-          </DialogActions>
-        </DialogBody>
-      </DialogSurface>
-    </Dialog>
+                  )}
+                />
+              </div>
+            </DialogContent>
+            <DialogActions>
+              <DialogTrigger disableButtonEnhancement>
+                <Button appearance="secondary">Cancelar</Button>
+              </DialogTrigger>
+              <Button
+                onClick={handleResetPassword}
+                disabled={reseting}
+                icon={reseting ? <Spinner size="tiny" /> : undefined}
+                appearance="primary"
+              >
+                Restablecer
+              </Button>
+            </DialogActions>
+          </DialogBody>
+        </DialogSurface>
+      </Dialog>
+      <Dialog
+        open={openSuccess}
+        onOpenChange={(_, e) => setOpenSuccess(e.open)}
+        modalType="modal"
+      >
+        <DialogSurface>
+          <DialogBody>
+            <DialogTitle>Contraseña restablecida correctamente</DialogTitle>
+            <DialogContent>
+              <div className="grid pt-5 gap-2">
+                <Field label="Usuario:" required>
+                  <p className="font-semibold">{user.email}</p>
+                </Field>
+                <Field label="Nueva contraseña:" required>
+                  <p className="font-semibold">{password}</p>
+                </Field>
+              </div>
+            </DialogContent>
+            <DialogActions>
+              <DialogTrigger disableButtonEnhancement>
+                <Button appearance="secondary">Cerrar</Button>
+              </DialogTrigger>
+              <Button
+                onClick={() => {
+                  navigator.clipboard.writeText(password)
+                  toast.success('Contraseña copiada al portapapeles ✅')
+                  setOpenSuccess(false)
+                  reset()
+                }}
+                appearance="secondary"
+                icon={<CopyRegular />}
+              >
+                <p className="text-nowrap">Copiar contraseña</p>
+              </Button>
+              <DialogTrigger disableButtonEnhancement>
+                <Button appearance="primary">Aceptar</Button>
+              </DialogTrigger>
+            </DialogActions>
+          </DialogBody>
+        </DialogSurface>
+      </Dialog>
+    </>
   )
 }
