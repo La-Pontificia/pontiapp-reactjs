7a5733357188bd0df809a1a40b3f8ac018fdb75d
Daustinn <daustinn.music@gmail.com>
2025-01-29 20:07:48 -0500

    Refactor routes.tsx: Add login and password reset functionality

---
+++ src/pages/routes.tsx
@@ -0,0 +1,51 @@
+import { Outlet, Route, Routes } from 'react-router'
+import RootLayout from '~/pages/+layout'
+import HomePage from '~/pages/+page'
+import LoginPage from '~/pages/login/+page'
+import AuthMiddleware from '~/middlewares/auth'
+import { AuthProvider } from '~/store/auth'
+import NotFounPage from '~/pages/not-fount'
+import SlugRoutes from '~/pages/slug/routes'
+import ModulesRoutes from '~/pages/modules/routes'
+import DocsLayout from './docs/+layout'
+import DocsPage from './docs/+page'
+import SlugDocsPage from './docs/slug/+page'
+import CreatePasswordPage from './create-password/+page'
+
+export default function MainRoutes() {
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+
+      <Route
+        element={
+          <AuthProvider>
+            <AuthMiddleware>
+              <Outlet />
+              {/* <RootLayout /> */}
+            </AuthMiddleware>
+          </AuthProvider>
+        }
+      >
+        <Route path="/create-password" element={<CreatePasswordPage />} />
+        <Route element={<RootLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="m/*" element={<ModulesRoutes />} />
+          <Route path="*" element={<SlugRoutes />} />
+        </Route>
+      </Route>
+      <Route
+        path="docs"
+        element={
+          <AuthProvider redirectWithoutSession={false}>
+            <DocsLayout />
+          </AuthProvider>
+        }
+      >
+        <Route index element={<DocsPage />} />
+        <Route path=":slug*" element={<SlugDocsPage />} />
+      </Route>
+      <Route path="*" element={<NotFounPage />} />
+    </Routes>
+  )
+}
