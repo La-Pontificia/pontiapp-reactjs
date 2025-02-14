030e8329bbe9d412fcf07a41171a4463e1ea5b0e
Daustinn <daustinn.music@gmail.com>
2025-02-14 13:30:53 -0500

    Refactor getPersonByDocumentId function: Update API endpoint and response handling; add fullName property to Person type

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,57 +1,26 @@
-import { VITE_APIRENIEC_HOST, VITE_APIRENIEC_TOKEN } from '~/config/env'
-
 type Person = {
   firstNames: string
   lastNames: string
   documentId: string
+  fullName: string
 }
 
 export async function getPersonByDocumentId(
-  documentId: string,
-  cache: boolean = true
+  documentId: string
 ): Promise<Person | null> {
   try {
-    const cacheKey = `person-${documentId}`
-    const cachedPerson = localStorage.getItem(cacheKey)
-    if (cachedPerson && cache) {
-      return JSON.parse(cachedPerson)
-    }
-
     const response = await fetch(
-      `${VITE_APIRENIEC_HOST}/queries/dni?number=${documentId}`,
-      {
-        headers: {
-          Authorization: `Bearer ${VITE_APIRENIEC_TOKEN}`
-        }
-      }
+      `https://graphperu.daustinn.com/api/query/${documentId}`
     )
-
     const data = await response.json()
 
-    if (data.status === false) {
-      return null
-    }
-
     const person = {
-      documentId: data.credentials.numeroDocumento,
-      firstNames: data.credentials.nombres
-        .split(' ')
-        .map(
-          (name: string) =>
-            name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()
-        )
-        .join(' '),
-      lastNames:
-        data.credentials.apellidoPaterno.charAt(0).toUpperCase() +
-        data.credentials.apellidoPaterno.slice(1).toLowerCase() +
-        ' ' +
-        data.credentials.apellidoMaterno.charAt(0).toUpperCase() +
-        data.credentials.apellidoMaterno.slice(1).toLowerCase()
+      documentId: data.documentId,
+      firstNames: data.surnames,
+      lastNames: data.names,
+      fullName: data.fullName
     }
 
-    if (data.status === true) {
-      localStorage.setItem(cacheKey, JSON.stringify(person))
-    }
     return person
   } catch (error) {
     console.error(error)
