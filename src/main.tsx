1ed8e610836e63e6c3f21ac203d2eca26c54ad70
Daustinn <daustinn.music@gmail.com>
2025-02-14 13:30:56 -0500

    Add content-grid class to index.css for enhanced background styling; simplify component hierarchy in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router'
-import FluentUIProvider from '~/providers/fluentui'
 import ThemeProvider from '~/providers/theme'
 import TanstackProvider from './providers/tanstack'
 import UiProvider from './providers/ui'
@@ -9,14 +8,12 @@ import MainRoutes from './pages/routes'
 
 createRoot(document.body).render(
   <ThemeProvider>
-    <FluentUIProvider>
-      <UiProvider>
-        <BrowserRouter>
-          <TanstackProvider>
-            <MainRoutes />
-          </TanstackProvider>
-        </BrowserRouter>
-      </UiProvider>
-    </FluentUIProvider>
+    <UiProvider>
+      <BrowserRouter>
+        <TanstackProvider>
+          <MainRoutes />
+        </TanstackProvider>
+      </BrowserRouter>
+    </UiProvider>
   </ThemeProvider>
 )
