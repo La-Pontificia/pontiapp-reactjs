d390f45e9d1bd8985be975efaffd8d4f0eb4772a
Daustinn <daustinn.music@gmail.com>
2025-04-07 01:43:21 -0500

    Remove attentions store and enhance auth provider with business unit management

--- src/store/auth.tsx
+++ src/store/auth.tsx
@@ -2,9 +2,13 @@
 import { api } from '~/lib/api'
 import { User } from '~/types/user'
 import React, { createContext } from 'react'
+import { BusinessUnit } from '~/types/rm/business-unit'
 
 interface AuthState {
   user: User
+  businessUnit?: BusinessUnit
+  businessUnits: BusinessUnit[]
+  handleToggleBusinessUnit: (item: BusinessUnit) => void
   setUser: React.Dispatch<React.SetStateAction<User | null>>
   privileges?: string[]
   loading?: boolean
@@ -27,30 +31,44 @@ export const AuthProvider = ({
   const [user, setUser] = React.useState<User | null>(null)
   const [birthdayBoys, setBirthdayBoys] = React.useState<User[]>([])
   const [loading, setLoading] = React.useState(true)
+  const [businessUnit, setBusinessUnit] = React.useState<BusinessUnit>()
+  const [businessUnits, setBusinessUnits] = React.useState<BusinessUnit[]>([])
 
   const fetchAuth = async () => {
     try {
       const res = await api.get<{
         authUser: User
         birthdayBoys: User[]
+        businessUnits: BusinessUnit[]
       }>('auth/current?relationship=userRole,role', {
         redirectWithoutSession
       })
 
       if (!res.ok) throw new Error(res.error)
 
-      const userInstance = new User(res.data.authUser)
-      const birthdayBoysInstances = res.data.birthdayBoys.map(
-        (user) => new User(user)
+      // get business unit id by localsotrage
+      const businessUnitInstance = res.data.businessUnits.find(
+        (bu) => bu.id === localStorage.getItem('businessUnitId')
       )
-      setBirthdayBoys(birthdayBoysInstances)
-      setUser(userInstance)
+
+      if (businessUnitInstance) {
+        setBusinessUnit(businessUnitInstance)
+      }
+
+      setBusinessUnits(res.data.businessUnits.map((bu) => new BusinessUnit(bu)))
+      setBirthdayBoys(res.data.birthdayBoys.map((user) => new User(user)))
+      setUser(new User(res.data.authUser))
     } catch (err) {
       console.error(err)
       setUser(null)
     } finally {
       setLoading(false)
     }
+  }
+
+  const handleToggleBusinessUnit = (item: BusinessUnit) => {
+    localStorage.setItem('businessUnitId', item.id)
+    setBusinessUnit(item)
   }
 
   const signOut = async () => {
@@ -76,8 +94,11 @@ export const AuthProvider = ({
       value={{
         user: user as User,
         loading,
+        handleToggleBusinessUnit,
         signOut,
         setUser,
+        businessUnit,
+        businessUnits,
         birthdayBoys
       }}
     >
