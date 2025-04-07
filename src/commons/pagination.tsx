07d2adc93deba96ec06118493045f6c5df49e20b
Daustinn <daustinn.music@gmail.com>
2025-04-07 01:39:36 -0500

    Enhance Pagination and SearchBox components with improved styling and functionality

--- src/commons/pagination.tsx
+++ src/commons/pagination.tsx
@@ -1,3 +1,4 @@
+import { ChevronLeftFilled, ChevronRightFilled } from '@fluentui/react-icons'
 import { ResponsePaginate } from '~/types/paginate-response'
 
 export default function Pagination({
@@ -10,18 +11,17 @@ export default function Pagination({
   const { current_page, from, links, next_page_url, prev_page_url, to, total } =
     state
   return (
-    <div className="flex py-2 max-lg:px-2 w-full items-center justify-start gap-3">
-      <footer className="flex items-center max-lg:w-full [&>button]:rounded-lg [&>button]:transition-all [&>button]:justify-center [&>button]:lg:flex [&>button]:gap-2 [&>button]:items-center [&>button]:first:px-2 [&>button]:last:px-3 overflow-hidden disabled:[&>button]:pointer-events-none disabled:[&>button]:opacity-50 dark:text-stone-300 text-stone-700">
+    <div className="flex w-full items-center justify-start gap-3">
+      <footer className="flex items-center gap-2 max-lg:w-full overflow-hidden disabled:[&>button]:!pointer-events-none disabled:[&>button]:!opacity-20 dark:text-stone-300 text-stone-700">
         <button
-          className="max-lg:mr-auto flex"
+          className="max-lg:mr-auto border dark:border-stone-500/30 rounded-md flex items-center gap-2 justify-center h-[30px] aspect-[16/14]"
           onClick={() => {
             onChangePage?.(current_page - 1)
           }}
           disabled={!prev_page_url}
         >
-          <p>Anterior</p>
+          <ChevronLeftFilled fontSize={25} />
         </button>
-        <p className="font-semibold flex lg:hidden">{current_page}</p>
         {links?.slice(1, links.length - 1).map((link, key) => {
           return (
             <button
@@ -32,29 +32,31 @@ export default function Pagination({
                 if (current_page === Number(link.label)) return
                 onChangePage?.(Number(link.label))
               }}
-              className="data-[active]:dark:text-blue-400 data-[active]:text-blue-600 max-lg:hidden font-medium"
+              className="w-[30px] aspect-square font-medium text-base data-[active]:dark:text-blue-400 data-[active]:dark:bg-blue-700/30 data-[active]:dark:border-blue-600 data-[active]:text-blue-700 data-[active]:bg-blue-400/10 data-[active]:border-blue-700 border dark:border-stone-500/30 rounded-md flex items-center justify-center"
             >
               {link.label}
             </button>
           )
         })}
         <button
-          className="max-lg:ml-auto flex"
+          className="max-lg:ml-auto border dark:border-stone-500/30 rounded-md flex items-center gap-2 justify-center h-[30px] aspect-[16/14]"
           onClick={() => {
             onChangePage?.(current_page + 1)
           }}
           disabled={!next_page_url}
         >
-          <p>Siguiente</p>
+          <ChevronRightFilled fontSize={20} />
         </button>
       </footer>
-      <p className="max-lg:hidden">
-        <span>{from}</span>
-        {'-'}
-        <span>{to}</span>
-        {' de '}
-        <span>{total}</span>
-      </p>
+      {from && to && total && (
+        <p className="max-lg:hidden font-medium text-sm">
+          <span>{from}</span>
+          {'-'}
+          <span>{to}</span>
+          {' de '}
+          <span>{total}</span>
+        </p>
+      )}
     </div>
   )
 }
--- src/commons/search-box.tsx
+++ src/commons/search-box.tsx
@@ -1,41 +1,69 @@
-import React from 'react'
-import { DismissFilled, SearchFilled } from '@fluentui/react-icons'
+import { DismissFilled } from '@fluentui/react-icons'
 import { cn } from '~/utils'
+
+import { useControllableState } from 'hothooks'
+import React from 'react'
 
 export const SearchBox = React.forwardRef<
   HTMLInputElement,
   React.InputHTMLAttributes<HTMLInputElement> & {
     dismiss?: () => void
+    onSearch?: (value: string | null) => void
   }
->(({ className, dismiss, ...props }, ref) => {
-  const [hover, setHover] = React.useState(false)
-  return (
-    <label className="flex relative group items-center gap-2">
-      <input
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-        ref={ref}
-        readOnly={!hover}
-        {...props}
-        className={cn(
-          'w-full bg-stone-500/20 peer text-[13px] font-medium outline-none rounded-lg px-3 placeholder:text-stone-500 placeholder:font-medium py-2 pl-8',
-          className
-        )}
-      />
-      <div className="absolute inset-y-0 peer-focus:opacity-100 peer-focus:dark:text-blue-500 opacity-50 flex items-center px-2">
-        <SearchFilled fontSize={20} />
-      </div>
-      <div className="absolute group-hover:opacity-100 opacity-0 inset-y-0 px-3 right-0 flex items-center">
-        <button
-          type="button"
-          className="text-stone-600 dark:text-stone-400"
-          onClick={dismiss}
-        >
-          <DismissFilled fontSize={18} />
-        </button>
-      </div>
-    </label>
-  )
-})
+>(
+  (
+    {
+      className,
+      dismiss,
+      defaultValue: defaultProp,
+      value: valueProp,
+      onChange,
+      onSearch,
+      ...props
+    },
+    ref
+  ) => {
+    const [hover, setHover] = React.useState(false)
+
+    const [value, setValue] = useControllableState<string>({
+      defaultProp: defaultProp?.toString(),
+      onChange: (v) => onSearch?.(v),
+      prop: valueProp?.toString()
+    })
+    return (
+      <label className="flex relative group items-center gap-2">
+        <input
+          {...props}
+          ref={ref}
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+          readOnly={!hover}
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value)
+            onChange?.(e)
+          }}
+          className={cn(
+            'w-full border bg-transparent border-stone-300 dark:border-stone-700 peer text-[13px] font-medium outline-none rounded-full px-3 placeholder:text-stone-500 placeholder:font-medium py-1.5',
+            className
+          )}
+        />
+        <div className="absolute group-hover:opacity-100 opacity-0 inset-y-0 px-3 right-0 flex items-center">
+          <button
+            type="button"
+            className="text-stone-600 dark:text-stone-400"
+            onClick={() => {
+              setValue('')
+              onSearch?.(null)
+              dismiss?.()
+            }}
+          >
+            <DismissFilled fontSize={18} />
+          </button>
+        </div>
+      </label>
+    )
+  }
+)
 SearchBox.displayName = 'SearchBox'
 export default SearchBox
