ec64358727016120d02745272e063805ccf3ebec
Daustinn <daustinn.music@gmail.com>
2025-05-05 01:53:33 -0500

    Refactor import paths across layout and page components to use absolute paths for consistency
    - Updated import statements in `+layout.tsx`, `+page.tsx`, and `routes.tsx` to utilize absolute paths.
    - Improved code readability and maintainability by standardizing import paths throughout the affected files.

--- src/pages/+layout.tsx
+++ src/pages/+layout.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router'
-import { useUi } from '~/store/ui'
+import { useUi } from '@/store/ui'
 import RootSidebar from './_components/sidebar'
 import RootHeader from './_components/header'
 import UserNotices from './_components/outsides/notices'
-import BirthdayBoys from '~/components/birthdays'
+import BirthdayBoys from '@/components/birthdays'
 import React from 'react'
-import { UIContext } from '~/providers/ui'
+import { UIContext } from '@/providers/ui'
 import { useMediaQuery } from '@uidotdev/usehooks'
 import BusinessUnitVerify from './_components/outsides/business-unit'
 
@@ -32,12 +32,15 @@ export default function RootLayout() {
       <BusinessUnitVerify />
       <div
         data-maximized={isModuleMaximized ? '' : undefined}
-        className="flex-grow h-full relative max-lg:pb-[65px] data-[maximized]:static flex lg:p-1 pl-0"
+        // lg:p-1 pl-0
+        className="flex-grow h-full relative max-lg:pb-[65px] data-[maximized]:static flex"
       >
         <div
           ref={ctxui?.contentRef}
           data-maximized={isModuleMaximized ? '' : undefined}
-          className="flex-grow relative max-lg:relative max-w-[calc(100vw-74px)] max-lg:max-w-[100%] data-[maximized]:max-w-full border dark:border-none lg:shadow-[0_0_6px_rgba(0,0,0,.1)] lg:dark:shadow-[0_0_6px_rgba(0,0,0,.5)] overflow-auto data-[maximized]:absolute data-[maximized]:z-[999] data-[maximized]:inset-0 max-lg:rounded-none rounded-xl data-[maximized]:rounded-none flex-col h-full flex overflow-y-auto w-full bg-[#f5f4f3] dark:bg-[#191a19] text-black dark:text-neutral-100"
+          // lg:shadow-[0_0_6px_rgba(0,0,0,.1)] lg:dark:shadow-[0_0_6px_rgba(0,0,0,.5)]
+          // max-lg:rounded-none rounded-xl
+          className="flex-grow relative max-lg:relative max-w-[calc(100vw-70px)] max-lg:max-w-[100%] data-[maximized]:max-w-full border dark:border-none overflow-auto data-[maximized]:absolute data-[maximized]:z-[999] data-[maximized]:inset-0  data-[maximized]:rounded-none flex-col h-full flex overflow-y-auto w-full bg-[#f5f4f3] dark:bg-[#141414] text-black dark:text-neutral-100"
         >
           <RootHeader />
           <Outlet />
