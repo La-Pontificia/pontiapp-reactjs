aef0903281fc01420a6754e9bea75eb3cbd6efc7
Daustinn <daustinn.music@gmail.com>
2025-05-07 09:03:25 -0500

    Refactor AcademicSidebar component to improve navigation structure. Update item labels for clarity and add new report files section based on user privileges. Remove unused sidebar items for a cleaner interface.

--- src/pages/modules/academic/sidebar.tsx
+++ src/pages/modules/academic/sidebar.tsx
@@ -28,7 +28,7 @@ import { useAuth } from '@/store/auth'
 export const AcademicSidebar = () => {
   const { user: authUser } = useAuth()
   return (
-    <ReusableSidebar homePath="/m/academic" title="Gestión Académica">
+    <ReusableSidebar homePath="/m/academic">
       <ItemSidebarNav
         icon={ChartMultipleRegular}
         iconActive={ChartMultipleFilled}
@@ -44,14 +44,7 @@ export const AcademicSidebar = () => {
       >
         Seguimiento docentes
       </ItemSidebarNav>
-      <ItemSidebarNav
-        has={['academic:schedules']}
-        icon={CalendarRegular}
-        iconActive={CalendarFilled}
-        href="/m/academic/schedules"
-      >
-        Horarios
-      </ItemSidebarNav>
+
       <ItemSidebarNav
         has={['academic:reportFiles']}
         icon={DocumentTableRegular}
@@ -61,77 +54,85 @@ export const AcademicSidebar = () => {
         Archivos de reportes
       </ItemSidebarNav>
       {authUser.hasPrivilege(
+        ['academic:schedules', 'academic:teacherSchedules'],
+        'or'
+      ) && <SidebarTitle>Horarios</SidebarTitle>}
+
+      <ItemSidebarNav
+        has={['academic:schedules']}
+        icon={CalendarRegular}
+        iconActive={CalendarFilled}
+        href="/m/academic/schedules"
+      >
+        Horarios de clase
+      </ItemSidebarNav>
+      <ItemSidebarNav
+        icon={CalendarPersonRegular}
+        iconActive={CalendarPersonFilled}
+        href="/m/academic/teacher-schedules"
+        has={['academic:teacherSchedules']}
+      >
+        Disponibilidad de los docentes
+      </ItemSidebarNav>
+      {authUser.hasPrivilege(
         [
           'academic:programs',
           'academic:periods',
           'academic:sections',
           'academic:courses',
           'academic:pavilionsClassrooms',
-          'academic:areas',
-          'academic:teacherSchedules'
+          'academic:areas'
         ],
         'or'
-      ) && (
-        <>
-          <SidebarTitle>Metadatos académicos</SidebarTitle>
-          <ItemSidebarNav
-            icon={CalendarPersonRegular}
-            iconActive={CalendarPersonFilled}
-            href="/m/academic/teacher-schedules"
-            has={['academic:teacherSchedules']}
-          >
-            Horarios docentes
-          </ItemSidebarNav>
-          <ItemSidebarNav
-            icon={HatGraduationRegular}
-            iconActive={HatGraduationFilled}
-            href="/m/academic/programs"
-            has={['academic:programs']}
-          >
-            Programas y planes
-          </ItemSidebarNav>
-          <ItemSidebarNav
-            icon={FolderListRegular}
-            iconActive={FolderListFilled}
-            href="/m/academic/periods"
-            has={['academic:periods']}
-          >
-            Periodos
-          </ItemSidebarNav>
-          <ItemSidebarNav
-            icon={FolderListRegular}
-            iconActive={FolderListFilled}
-            href="/m/academic/sections"
-            has={['academic:sections']}
-          >
-            Secciones
-          </ItemSidebarNav>
-          <ItemSidebarNav
-            icon={BookSearchRegular}
-            iconActive={BookSearchFilled}
-            href="/m/academic/courses"
-            has={['academic:courses']}
-          >
-            Cursos
-          </ItemSidebarNav>
-          <ItemSidebarNav
-            icon={BuildingRegular}
-            iconActive={BuildingFilled}
-            href="/m/academic/classrooms"
-            has={['academic:pavilionsClassrooms']}
-          >
-            Aulas y pabellones
-          </ItemSidebarNav>
-          <ItemSidebarNav
-            icon={FolderListRegular}
-            iconActive={FolderListFilled}
-            href="/m/academic/areas"
-            has={['academic:areas']}
-          >
-            Áreas
-          </ItemSidebarNav>
-        </>
-      )}
+      ) && <SidebarTitle>Datos y ajustes</SidebarTitle>}
+      <ItemSidebarNav
+        icon={HatGraduationRegular}
+        iconActive={HatGraduationFilled}
+        href="/m/academic/programs"
+        has={['academic:programs']}
+      >
+        Programas y planes
+      </ItemSidebarNav>
+      <ItemSidebarNav
+        icon={FolderListRegular}
+        iconActive={FolderListFilled}
+        href="/m/academic/periods"
+        has={['academic:periods']}
+      >
+        Periodos
+      </ItemSidebarNav>
+      <ItemSidebarNav
+        icon={FolderListRegular}
+        iconActive={FolderListFilled}
+        href="/m/academic/sections"
+        has={['academic:sections']}
+      >
+        Secciones
+      </ItemSidebarNav>
+      <ItemSidebarNav
+        icon={BookSearchRegular}
+        iconActive={BookSearchFilled}
+        href="/m/academic/courses"
+        has={['academic:courses']}
+      >
+        Cursos
+      </ItemSidebarNav>
+      <ItemSidebarNav
+        icon={BuildingRegular}
+        iconActive={BuildingFilled}
+        href="/m/academic/classrooms"
+        has={['academic:pavilionsClassrooms']}
+      >
+        Aulas y pabellones
+      </ItemSidebarNav>
+      <ItemSidebarNav
+        icon={FolderListRegular}
+        iconActive={FolderListFilled}
+        href="/m/academic/areas"
+        has={['academic:areas']}
+      >
+        Áreas
+      </ItemSidebarNav>
     </ReusableSidebar>
   )
 }
