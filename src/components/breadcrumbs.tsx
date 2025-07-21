2c588c5d31552befed738878f3f8f94fa8514ad8
Daustinn <daustinn.music@gmail.com>
2025-07-21 04:15:01 -0500

    refactor: standardize import statements and improve code formatting in breadcrumbs component

--- src/components/breadcrumbs.tsx
+++ src/components/breadcrumbs.tsx
@@ -41,7 +41,7 @@ export type BreadcrumbType = {
 //   )
 // }
 
-import * as React from "react";
+import * as React from 'react'
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -60,58 +60,61 @@ import {
   Tooltip,
   truncateBreadcrumbLongName,
   isTruncatableBreadcrumbContent,
-  MenuItem,
-} from "@fluentui/react-components";
+  MenuItem
+} from '@fluentui/react-components'
 import {
   MoreHorizontalRegular,
   MoreHorizontalFilled,
-  bundleIcon,
-} from "@fluentui/react-icons";
-import type { PartitionBreadcrumbItems } from "@fluentui/react-components";
-import { NavigateFunction, useNavigate } from "react-router";
-import { capitalizeText } from "@/utils";
+  bundleIcon
+} from '@fluentui/react-icons'
+import type { PartitionBreadcrumbItems } from '@fluentui/react-components'
+import { NavigateFunction, useNavigate } from 'react-router'
+import { capitalizeText } from '@/utils'
 
-const MoreHorizontal = bundleIcon(MoreHorizontalFilled, MoreHorizontalRegular);
+const MoreHorizontal = bundleIcon(MoreHorizontalFilled, MoreHorizontalRegular)
 
 type Item = {
-  key: number;
-  item?: string;
+  key: number
+  item?: string
   itemProps?: {
-    icon?: ButtonProps["icon"];
-    disabled?: boolean;
-    href?: string;
-  };
-};
+    icon?: ButtonProps['icon']
+    disabled?: boolean
+    href?: string
+  }
+}
 
 const useTooltipStyles = makeStyles({
   tooltip: {
-    whiteSpace: "nowrap",
-    overflow: "hidden",
-    textOverflow: "ellipsis",
-  },
-});
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis'
+  }
+})
 
 const OverflowGroupDivider: React.FC<{
-  groupId: number;
+  groupId: number
 }> = (props) => {
   return (
     <OverflowDivider groupId={props.groupId.toString()}>
       <BreadcrumbDivider data-group={props.groupId} />
     </OverflowDivider>
-  );
-};
+  )
+}
 
-const renderBreadcrumbItem = (el: Item, isLastItem: boolean = false, navigate: NavigateFunction) => {
-
-
+const renderBreadcrumbItem = (
+  el: Item,
+  isLastItem: boolean = false,
+  navigate: NavigateFunction
+) => {
   const buttonProps = {
     ...el.itemProps,
     onClick: (e: any) => {
-      e.preventDefault();
-      e.stopPropagation();
-      navigate(el.itemProps?.href ?? "")
+      e.preventDefault()
+      e.stopPropagation()
+      navigate(el.itemProps?.href ?? '')
     },
-    className: "!text-lg data-[first]:!font-semibold data-[first]:!text-xl font-medium dark:data-[first]:!text-blue-400 data-[first]:!text-blue-700",
+    className:
+      '!text-lg data-[first]:!font-semibold data-[first]:!text-xl font-medium dark:data-[first]:!text-blue-400 data-[first]:!text-blue-700',
     'data-first': el.key === 0 ? '' : undefined
   }
 
@@ -119,63 +122,69 @@ const renderBreadcrumbItem = (el: Item, isLastItem: boolean = false, navigate: N
     <React.Fragment key={`button-items-${el.key}`}>
       {isTruncatableBreadcrumbContent(el.item ?? '', 20) ? (
         <BreadcrumbItem>
-          <Tooltip withArrow content={capitalizeText(el.item ?? '')} relationship="label">
-            <BreadcrumbButton
-              {...buttonProps}
-              current={isLastItem}>
+          <Tooltip
+            withArrow
+            content={capitalizeText(el.item ?? '')}
+            relationship="label"
+          >
+            <BreadcrumbButton {...buttonProps} current={isLastItem}>
               {truncateBreadcrumbLongName(capitalizeText(el.item ?? ''), 20)}
             </BreadcrumbButton>
           </Tooltip>
         </BreadcrumbItem>
       ) : (
         <BreadcrumbItem>
-          <BreadcrumbButton  {...buttonProps} current={isLastItem}>{el.item}</BreadcrumbButton>
+          <BreadcrumbButton {...buttonProps} current={isLastItem}>
+            {el.item}
+          </BreadcrumbButton>
         </BreadcrumbItem>
       )}
       {!isLastItem && <OverflowGroupDivider groupId={el.key} />}
     </React.Fragment>
-  );
-};
+  )
+}
 
 const getTooltipContent = (breadcrumbItems: readonly Item[] | undefined) => {
   if (!breadcrumbItems) {
-    return "";
+    return ''
   }
   return breadcrumbItems.reduce((acc, initialValue, _, arr) => {
     return (
       <>
         {acc}
-        {arr[0].item !== initialValue.item && " > "}
+        {arr[0].item !== initialValue.item && ' > '}
         {initialValue.item}
       </>
-    );
-  }, <React.Fragment />);
-};
+    )
+  }, <React.Fragment />)
+}
 
-const OverflowMenu = (props: PartitionBreadcrumbItems<Item> & {
-  navigate: NavigateFunction;
-}) => {
-  const { overflowItems, startDisplayedItems, endDisplayedItems, navigate } = props;
+const OverflowMenu = (
+  props: PartitionBreadcrumbItems<Item> & {
+    navigate: NavigateFunction
+  }
+) => {
+  const { overflowItems, startDisplayedItems, endDisplayedItems, navigate } =
+    props
   const { ref, isOverflowing, overflowCount } =
-    useOverflowMenu<HTMLButtonElement>();
+    useOverflowMenu<HTMLButtonElement>()
 
-  const tooltipStyles = useTooltipStyles();
+  const tooltipStyles = useTooltipStyles()
 
   if (!isOverflowing && overflowItems && overflowItems.length === 0) {
-    return null;
+    return null
   }
 
   const overflowItemsCount = overflowItems
     ? overflowItems.length + overflowCount
-    : overflowCount;
+    : overflowCount
   const tooltipContent =
     overflowItemsCount > 3
       ? `${overflowItemsCount} items`
       : {
-        children: getTooltipContent(overflowItems),
-        className: tooltipStyles.tooltip,
-      };
-
+          children: getTooltipContent(overflowItems),
+          className: tooltipStyles.tooltip
+        }
 
   return (
     <BreadcrumbItem>
@@ -198,11 +207,13 @@ const OverflowMenu = (props: PartitionBreadcrumbItems<Item> & {
               startDisplayedItems.map((item: Item) => (
                 <MenuItem
                   onClick={(e) => {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    navigate(item.itemProps?.href ?? "")
+                    e.preventDefault()
+                    e.stopPropagation()
+                    navigate(item.itemProps?.href ?? '')
                   }}
-                  id={item.key.toString()} key={item.key} >
+                  id={item.key.toString()}
+                  key={item.key}
+                >
                   {item.item}
                 </MenuItem>
               ))}
@@ -210,9 +221,9 @@ const OverflowMenu = (props: PartitionBreadcrumbItems<Item> & {
               overflowItems.map((item: Item) => (
                 <MenuItem
                   onClick={(e) => {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    navigate(item.itemProps?.href ?? "")
+                    e.preventDefault()
+                    e.stopPropagation()
+                    navigate(item.itemProps?.href ?? '')
                   }}
                   id={item.key.toString()}
                   key={item.key}
@@ -225,9 +236,9 @@ const OverflowMenu = (props: PartitionBreadcrumbItems<Item> & {
               endDisplayedItems.map((item: Item) => (
                 <MenuItem
                   onClick={(e) => {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    navigate(item.itemProps?.href ?? "")
+                    e.preventDefault()
+                    e.stopPropagation()
+                    navigate(item.itemProps?.href ?? '')
                   }}
                   id={item.key.toString()}
                   key={item.key}
@@ -239,38 +250,37 @@ const OverflowMenu = (props: PartitionBreadcrumbItems<Item> & {
         </MenuPopover>
       </Menu>
     </BreadcrumbItem>
-  );
-};
+  )
+}
 
 const BreadcrumbOverflowExample = ({
-  breadcrumbs,
+  breadcrumbs
 }: {
   breadcrumbs: BreadcrumbType[]
 }) => {
   const {
     startDisplayedItems,
     overflowItems,
-    endDisplayedItems,
+    endDisplayedItems
   }: PartitionBreadcrumbItems<Item> = partitionBreadcrumbItems({
     items: breadcrumbs.map((item, index) => ({
       key: index,
       item: item.name,
       itemProps: {
-        href: item.to ?? '',
+        href: item.to ?? ''
       }
     })),
-    maxDisplayedItems: 4,
-  });
+    maxDisplayedItems: 4
+  })
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   return (
     <Breadcrumb size="large" className="grow">
       {startDisplayedItems.map((item: Item) => {
-        const isLastItem = item.key === breadcrumbs.length - 1;
+        const isLastItem = item.key === breadcrumbs.length - 1
         return renderBreadcrumbItem(item, isLastItem, navigate)
-      }
-      )}
+      })}
       {overflowItems && (
         <>
           <OverflowMenu
@@ -284,12 +294,11 @@ const BreadcrumbOverflowExample = ({
       )}
       {endDisplayedItems &&
         endDisplayedItems.map((item: Item) => {
-          const isLastItem = item.key === breadcrumbs.length - 1;
-          return renderBreadcrumbItem(item, isLastItem, navigate);
+          const isLastItem = item.key === breadcrumbs.length - 1
+          return renderBreadcrumbItem(item, isLastItem, navigate)
         })}
     </Breadcrumb>
-  );
-};
+  )
+}
 
-
-export default BreadcrumbOverflowExample;
+export default BreadcrumbOverflowExample
