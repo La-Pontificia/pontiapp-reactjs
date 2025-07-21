5ccc587abe846d2c79c268ac73ff7fd027bc9312
Daustinn <daustinn.music@gmail.com>
2025-07-21 03:56:33 -0500

    Comment out Echo and Pusher imports and related code in echo.ts

--- src/lib/echo.ts
+++ src/lib/echo.ts
@@ -1,29 +1,29 @@
-import Echo from 'laravel-echo'
-import Pusher from 'pusher-js'
-import {
-  VITE_REVERB_APP_KEY,
-  VITE_REVERB_HOST,
-  VITE_REVERB_PORT,
-  VITE_REVERB_SCHEME
-} from '@/config/env'
+// import Echo from 'laravel-echo'
+// import Pusher from 'pusher-js'
+// import {
+//   VITE_REVERB_APP_KEY,
+//   VITE_REVERB_HOST,
+//   VITE_REVERB_PORT,
+//   VITE_REVERB_SCHEME
+// } from '@/config/env'
 
-declare global {
-  interface Window {
-    Pusher: typeof Pusher
-  }
-}
+// declare global {
+//   interface Window {
+//     Pusher: typeof Pusher
+//   }
+// }
 
-window.Pusher = Pusher
+// window.Pusher = Pusher
 
-const echo = new Echo({
-  broadcaster: 'reverb',
-  key: VITE_REVERB_APP_KEY as string,
-  wsHost: VITE_REVERB_HOST as string,
-  wsPort: parseInt(VITE_REVERB_PORT ?? '80'),
-  wssPort: parseInt(VITE_REVERB_PORT ?? '443'),
-  forceTLS: (VITE_REVERB_SCHEME ?? 'https') === 'https',
-  enabledTransports: ['ws', 'wss'],
-  disableStats: true
-})
+// const echo = new Echo({
+//   broadcaster: 'reverb',
+//   key: VITE_REVERB_APP_KEY as string,
+//   wsHost: VITE_REVERB_HOST as string,
+//   wsPort: parseInt(VITE_REVERB_PORT ?? '80'),
+//   wssPort: parseInt(VITE_REVERB_PORT ?? '443'),
+//   forceTLS: (VITE_REVERB_SCHEME ?? 'https') === 'https',
+//   enabledTransports: ['ws', 'wss'],
+//   disableStats: true
+// })
 
-export default echo
+// export default echo
