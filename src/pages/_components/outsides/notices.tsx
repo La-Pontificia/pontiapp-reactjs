747008a9de3e7ab585ccee48e4ed1f6148e03dae
Daustinn <daustinn.music@gmail.com>
2025-07-21 03:56:38 -0500

    Comment out unused imports and related code in UserNotices component

--- src/pages/_components/outsides/notices.tsx
+++ src/pages/_components/outsides/notices.tsx
@@ -1,68 +1,57 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import {
-  Toast,
-  ToastBody,
-  Toaster,
-  ToastFooter,
-  ToastTitle,
-  Link as FluentLink,
-  ToastTrigger,
-  useId,
-  useToastController
-} from '@fluentui/react-components'
+import { Toaster, useId } from '@fluentui/react-components'
 import React from 'react'
-import { Link } from 'react-router'
-import echo from '@/lib/echo'
-import { useAuth } from '@/store/auth'
+// import { Link } from 'react-router'
+// import echo from '@/lib/echo'
+// import { useAuth } from '@/store/auth'
 
-type UserNotice = {
-  title: string
-  description: string
-  actions: { [key: string]: string }
-}
+// type UserNotice = {
+//   title: string
+//   description: string
+//   actions: { [key: string]: string }
+// }
 
 export default function UserNotices() {
-  const { user } = useAuth()
+  // const { user } = useAuth()
   const toasterId = useId('toaster-user-notice')
-  const { dispatchToast } = useToastController(toasterId)
+  // const { dispatchToast } = useToastController(toasterId)
 
   React.useEffect(() => {
-    echo.channel(`auth.${user.id}`).listen('.notice', (data: UserNotice) => {
-      notify(data)
-    })
+    // echo.channel(`auth.${user.id}`).listen('.notice', (data: UserNotice) => {
+    //   notify(data)
+    // })
   }, [])
 
-  const notify = (notice: UserNotice) =>
-    dispatchToast(
-      <Toast>
-        <ToastTitle
-          action={
-            <ToastTrigger>
-              <FluentLink>Cerrar</FluentLink>
-            </ToastTrigger>
-          }
-        >
-          {notice.title}
-        </ToastTitle>
-        <ToastBody subtitle={`Para ${user.displayName}`}>
-          {notice.description}
-        </ToastBody>
-        <ToastFooter>
-          {Object.entries(notice.actions).map(([key, path]) => (
-            <ToastTrigger key={key}>
-              <Link
-                className="dark:text-blue-500 hover:underline"
-                to={path}
-                target={path.startsWith('http') ? '_blank' : ''}
-              >
-                {key}
-              </Link>
-            </ToastTrigger>
-          ))}
-        </ToastFooter>
-      </Toast>,
-      { intent: 'success' }
-    )
+  // const notify = (notice: UserNotice) =>
+  //   dispatchToast(
+  //     <Toast>
+  //       <ToastTitle
+  //         action={
+  //           <ToastTrigger>
+  //             <FluentLink>Cerrar</FluentLink>
+  //           </ToastTrigger>
+  //         }
+  //       >
+  //         {notice.title}
+  //       </ToastTitle>
+  //       <ToastBody subtitle={`Para ${user.displayName}`}>
+  //         {notice.description}
+  //       </ToastBody>
+  //       <ToastFooter>
+  //         {Object.entries(notice.actions).map(([key, path]) => (
+  //           <ToastTrigger key={key}>
+  //             <Link
+  //               className="dark:text-blue-500 hover:underline"
+  //               to={path}
+  //               target={path.startsWith('http') ? '_blank' : ''}
+  //             >
+  //               {key}
+  //             </Link>
+  //           </ToastTrigger>
+  //         ))}
+  //       </ToastFooter>
+  //     </Toast>,
+  //     { intent: 'success' }
+  //   )
   return (
     <>
       <Toaster toasterId={toasterId} />
